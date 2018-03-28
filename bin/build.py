87e2a4e579eea56fbc573d933882b5f4296ef8b4
Matt Patrick <hazmat345@gmail.com>
2018-03-28 15:11:54 -0400

    Updating rpm build script

--- bin/build.py
+++ bin/build.py
@@ -7,6 +7,7 @@ import os
 
 BUILD_IMAGE = "bgio/build"
 SUPPORTED_DISTRIBUTIONS = ['centos6', 'centos7']
+SUPPORTED_PYTHONS = ['python2', 'python3']
 BUILD_TYPES = ['rpm']
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -18,11 +19,12 @@ def parse_args(cli_args):
     parser = argparse.ArgumentParser(description="Build beer-garden artifacts.")
     parser.add_argument('type', choices=BUILD_TYPES)
     parser.add_argument('--distribution', choices=SUPPORTED_DISTRIBUTIONS)
+    parser.add_argument('--python', choices=SUPPORTED_PYTHONS)
     parser.add_argument('--local', action='store_true', default=False)
     return parser.parse_args(cli_args)
 
 
-def build_rpms(cli_dist, local):
+def build_rpms(cli_dist, cli_python, local):
 
     if cli_dist:
         if cli_dist not in SUPPORTED_DISTRIBUTIONS:
@@ -34,8 +36,17 @@ def build_rpms(cli_dist, local):
     else:
         build_dists = SUPPORTED_DISTRIBUTIONS
 
+    build_python = cli_python or 'python3'
+    if build_python not in SUPPORTED_PYTHONS:
+        print("Invalid python (%s) for RPM build" % cli_python)
+        print("Supported distributions are: %s" % SUPPORTED_PYTHONS)
+        sys.exit(1)
+
     for dist in build_dists:
-        cmd = ["docker", "run", "-v", SRC_PATH + ":/src", "--rm", BUILD_IMAGE+':'+dist, RPM_BUILD_SCRIPT, "-r", dist[-1]]
+        cmd = ["docker", "run", "--rm",
+               "-v", SRC_PATH + ":/src",
+               BUILD_IMAGE+':'+dist+'-'+build_python,
+               RPM_BUILD_SCRIPT, "-r", dist[-1]]
 
         if local:
             cmd += ["--local"]
@@ -46,7 +57,7 @@ def build_rpms(cli_dist, local):
 def main():
     args = parse_args(sys.argv[1:])
     if args.type == 'rpm':
-        build_rpms(args.distribution, args.local)
+        build_rpms(args.distribution, args.python, args.local)
     else:
         print("Unsupported build type %s" % args.type)
         sys.exit(1)
