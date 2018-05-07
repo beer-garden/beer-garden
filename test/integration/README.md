9989a36418eabcd1de1a2217c2226fe376d67de8
Matt Patrick <hazmat345@gmail.com>
2018-05-07 15:02:07 -0400

    Running both sets of tests in travis

--- test/integration/README.md
+++ test/integration/README.md
@@ -8,3 +8,7 @@ These tests are designed to be run on Travis. The setup will install the require
 ## Running Locally
 
 If you're running Beer-garden locally you can also run these tests yourself. Just modify `rest/config.json` to tell the tests where to find your Beer-garden and make sure you have `pytest` installed. Run `pytest rest` to kick things off.
+
+## Parts
+- Lifecycle: These test that bartender and brew-view can correctly start up and shut down
+- Rest: These test the brew-view rest interface
---
+++ test/integration/lifecycle/all_test.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import time
+
+import pytest
+
+# This is the recommended import pattern, see https://github.com/google/python-subprocess32
+if os.name == 'posix' and sys.version_info[0] < 3:
+    import subprocess32 as subprocess
+else:
+    import subprocess
+
+
+@pytest.mark.timeout(20)
+class TestShutdown(object):
+
+    @pytest.mark.parametrize('command', [
+        ('brew-view',),
+        ('bartender',),
+    ])
+    def test_solo(self, command):
+        proc = subprocess.Popen(command)
+        time.sleep(3)
+        proc.terminate()
+
+        proc.wait()
+
+        assert proc.poll() == 0
+
+    def test_both(self):
+        bv_proc = subprocess.Popen('brew-view')
+        bt_proc = subprocess.Popen('bartender')
+
+        time.sleep(10)
+
+        bt_proc.terminate()
+        bt_proc.wait()
+        assert bt_proc.poll() == 0
+
+        bv_proc.terminate()
+        bv_proc.wait()
+        assert bv_proc.poll() == 0
