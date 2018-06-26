fbac095a901300568a32409d36f0ab18deca52a9
Matt Patrick <hazmat345@gmail.com>
2018-06-26 12:33:31 -0400

    Adding authorized decorator, permissions to token

--- brew_view/authorization.py
+++ brew_view/authorization.py
@@ -1,12 +1,46 @@
 import base64
+import functools
 from datetime import datetime, timedelta
 
 import jwt
 from mongoengine.errors import DoesNotExist
 from passlib.apps import custom_app_context
+from tornado.web import HTTPError
 
 import brew_view
 from bg_utils.models import Principal
+from brewtils.models import (
+    Principal as BrewtilsPrincipal,
+    Role as BrewtilsRole
+)
+
+
+def authenticated(method=None, permissions=None):
+    """Decorate methods with this to require various permissions"""
+
+    if method is None:
+        return functools.partial(authenticated, permissions=permissions)
+
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        # If no user don't even bother
+        if not self.current_user:
+            raise HTTPError(403)
+
+        # User permissions must contain ALL specified permissions
+        if not has_permission(self.current_user, permissions):
+            raise HTTPError(403)
+
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def has_permission(principal, required_permissions):
+    if 'all' in principal.permissions:
+        return True
+
+    return set(required_permissions).issubset(principal.permissions)
 
 
 def generate_token(principal):
@@ -16,7 +50,9 @@ def generate_token(principal):
         'sub': str(principal.id),
         'iat': current_time,
         'exp': current_time + timedelta(minutes=20),
+        'username': principal.username,
         'roles': [role['name'] for role in principal.roles],
+        'permissions': principal.permissions,
     }
     return jwt.encode(payload,
                       brew_view.tornado_app.settings["cookie_secret"],
@@ -27,14 +63,14 @@ def basic_auth(auth_header):
     auth_decoded = base64.b64decode(auth_header[6:]).decode()
     username, password = auth_decoded.split(':')
 
-    # In this case return 403 to prevent an attacker from being able to
-    # enumerate a list of user names
     try:
         principal = Principal.objects.get(username=username)
 
         if custom_app_context.verify(password, principal.hash):
             return principal
     except DoesNotExist:
+        # Don't handle this differently to prevent an attacker from being able
+        #  to enumerate a list of user names
         pass
 
     return None
@@ -47,9 +83,9 @@ def bearer_auth(auth_header):
                          brew_view.tornado_app.settings["cookie_secret"],
                          algorithm='HS256')
 
-    try:
-        return Principal.objects.get(id=decoded['sub'])
-    except DoesNotExist:
-        pass
-
-    return None
+    return BrewtilsPrincipal(
+        id=decoded['sub'],
+        username=decoded.get('username', ''),
+        roles=[BrewtilsRole(name=role) for role in decoded.get('roles', [])],
+        permissions=decoded.get('permissions', [])
+    )
