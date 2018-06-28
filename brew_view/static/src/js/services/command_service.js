3cf61dc509579a20aed8e51dc793009a50fcb44e
Matt Patrick <hazmat345@gmail.com>
2018-06-28 19:44:21 -0400

    Just requesting all systems on the command index page

--- brew_view/static/src/js/services/command_service.js
+++ brew_view/static/src/js/services/command_service.js
@@ -9,40 +9,12 @@ commandService.$inject = ['$http', '$rootScope', 'SystemService'];
  * @return {Object}               Service for interacting with the command API.
  */
 export default function commandService($http, $rootScope, SystemService) {
-  let CommandService = {};
-
-  CommandService.getSystemName = function(command) {
-    const system = CommandService.findSystem(command);
-    return system.display_name || system.name;
-  },
-
-  CommandService.findSystem = function(command) {
-    const systemId = command.system.id;
-    for ( let index in $rootScope.systems ) {
-      if ( systemId == $rootScope.systems[index].id ) {
-        return $rootScope.systems[index];
-      }
-    }
-  },
-
-  CommandService.getCommands = function() {
-    return $http.get('api/v1/commands');
-  },
-
-  CommandService.getCommand = function(id) {
-    return $http.get('api/v1/commands/' + id);
-  },
-
-  CommandService.comparison = function(a, b) {
-    const aSystem = CommandService.getSystemName(a);
-    const bSystem = CommandService.getSystemName(b);
-
-    if (aSystem < bSystem) return -1;
-    if (aSystem > bSystem) return 1;
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
-    return 0;
+  return {
+    getCommands: function() {
+      return $http.get('api/v1/commands');
+    },
+    getCommand: function(id) {
+      return $http.get('api/v1/commands/' + id);
+    },
   };
-
-  return CommandService;
 };
