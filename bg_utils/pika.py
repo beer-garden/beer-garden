17c90032a5c8c0cae5a634344f92849c3a0749da
Matt Patrick <hazmat345@gmail.com>
2018-08-07 16:30:18 -0400

    Moving pika ClientBase into brewtils

--- bg_utils/pika.py
+++ bg_utils/pika.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
 
 import logging
-import ssl as pyssl
 
-from pika import BasicProperties, ConnectionParameters, PlainCredentials, SSLOptions
+from pika import BasicProperties
 from pika import BlockingConnection
 from pika.exceptions import AMQPError
+
+from brewtils.pika import ClientBase
 
 
 def get_routing_key(*args, **kwargs):
@@ -53,84 +54,6 @@ def get_routing_keys(*args, **kwargs):
         routing_keys.append(routing_keys[-1] + '.' + new_key if len(routing_keys) else new_key)
 
     return routing_keys
-
-
-class ClientBase(object):
-    """Base class for connection to RabbitMQ."""
-
-    def __init__(
-            self,
-            host='localhost',
-            port=5672,
-            user='guest',
-            password='guest',
-            connection_attempts=3,
-            heartbeat_interval=3600,
-            virtual_host='/',
-            exchange='beer_garden',
-            ssl=None
-    ):
-
-        self._host = host
-        self._port = port
-        self._user = user
-        self._password = password
-        self._connection_attempts = connection_attempts
-        self._heartbeat_interval = heartbeat_interval
-        self._virtual_host = virtual_host
-        self._exchange = exchange
-
-        ssl = ssl or {}
-        self._ssl_enabled = ssl.get('enabled', False)
-        self._ssl_options = SSLOptions(
-            cafile=ssl.get('ca_cert', None),
-            verify_mode=pyssl.CERT_REQUIRED if ssl.get('ca_verify') else pyssl.CERT_NONE,
-        )
-
-        # Save off the 'normal' connection params so they don't need to be constructed every time
-        self._conn_params = self.connection_parameters()
-
-    @property
-    def connection_url(self):
-        """str: Connection URL for this client's connection information"""
-
-        virtual_host = self._conn_params.virtual_host
-        if virtual_host == '/':
-            virtual_host = ''
-
-        return 'amqp%s://%s:%s@%s:%s/%s' % \
-               ('s' if self._ssl_enabled else '',
-                self._conn_params.credentials.username,
-                self._conn_params.credentials.password,
-                self._conn_params.host,
-                self._conn_params.port,
-                virtual_host)
-
-    def connection_parameters(self, **kwargs):
-        """Get ``ConnectionParameters`` associated with this client
-
-        Will construct a ``ConnectionParameters`` object using parameters passed at initialization
-        as defaults. Any parameters passed in \*\*kwargs will override initialization parameters.
-
-        Args:
-            **kwargs: Overrides for specific parameters
-
-        Returns:
-            :obj:`pika.ConnectionParameters`: Generated ConnectionParameters object
-        """
-        credentials = PlainCredentials(username=kwargs.get('user', self._user),
-                                       password=kwargs.get('password', self._password))
-
-        return ConnectionParameters(host=kwargs.get('host', self._host),
-                                    port=kwargs.get('port', self._port),
-                                    ssl=kwargs.get('ssl_enabled', self._ssl_enabled),
-                                    ssl_options=kwargs.get('ssl_options', self._ssl_options),
-                                    virtual_host=kwargs.get('virtual_host', self._virtual_host),
-                                    connection_attempts=kwargs.get('connection_attempts',
-                                                                   self._connection_attempts),
-                                    heartbeat_interval=kwargs.get('heartbeat_interval',
-                                                                  self._heartbeat_interval),
-                                    credentials=credentials)
 
 
 class TransientPikaClient(ClientBase):
--- test/unit/pika_test.py
+++ test/unit/pika_test.py
@@ -3,37 +3,12 @@ from mock import ANY, MagicMock, Mock, PropertyMock, call
 from pika.exceptions import AMQPError
 
 import bg_utils.pika
-from bg_utils.pika import ClientBase, TransientPikaClient, get_routing_key, get_routing_keys
+from bg_utils.pika import TransientPikaClient, get_routing_key, get_routing_keys
 
 host = 'localhost'
 port = 5672
 user = 'user'
 password = 'password'
-
-
-class TestClientBase(object):
-
-    @pytest.fixture
-    def client(self):
-        return ClientBase(host=host, port=port, user=user, password=password)
-
-    def test_connection_parameters(self, client):
-        params = client.connection_parameters()
-        assert params.host == host
-        assert params.port == port
-
-        params = client.connection_parameters(host='another_host')
-        assert params.host == 'another_host'
-        assert params.port == port
-
-    def test_connection_url(self, client):
-        url = client.connection_url
-
-        assert url.startswith('amqp://') is True
-        assert user in url
-        assert password in url
-        assert host in url
-        assert str(port) in url
 
 
 class TestTransientPikaClient(object):
