58d169b0a5fbb66be44b43d3b22fd9a9552b3a33
Matt Patrick <hazmat345@gmail.com>
2018-08-10 23:06:28 -0400

    Command index page using new directive

--- brew_view/static/src/js/controllers/command_index.js
+++ brew_view/static/src/js/controllers/command_index.js
@@ -24,48 +24,6 @@ export default function commandIndexController(
   SystemService,
   CommandService,
   DTOptionsBuilder) {
-
-  $scope.commands = {
-    data: [],
-    loaded: false,
-    status: null,
-    error: false,
-    errorMessage: '',
-    errorMap: {
-      'empty': {
-        'solutions': [
-          {
-            problem: 'Backend Down',
-            description: 'If the backend is down, there will be no commands to control',
-            resolution: '<kbd>service bartender start</kbd>',
-          },
-          {
-            problem: 'Plugin Problems',
-            description: 'If Plugins attempted to start, but are failing to startup, then' +
-                         'you\'ll have to contact the plugin maintainer. You can tell what\'s '+
-                         'wrong by their logs. Plugins are located at ' +
-                         '<code>$APP_HOME/plugins</code>',
-            resolution: '<kbd>less $APP_HOME/log/my-plugin.log</kbd>',
-          },
-          {
-            problem: 'Database Names Do Not Match',
-            description: 'It is possible that the backend is pointing to a Different Database ' +
-                         'than the Frontend. Check to make sure that the <code>DB_NAME</code> ' +
-                         'in both config files is the same',
-            resolution: '<kbd>vim $APP_HOME/conf/bartender.json</kbd><br />' +
-                        '<kbd>vim $APP_HOME/conf/brew-view.json</kbd>',
-          },
-          {
-            problem: 'There Are No Commands',
-            description: 'If no one has ever developed any plugins, then there will be no ' +
-                         'systems here. You\'ll need to build your own plugins.',
-            resolution: 'Develop a Plugin',
-          },
-        ],
-      },
-    },
-  };
-
   $scope.dtOptions = DTOptionsBuilder.newOptions()
     .withOption('order', [4, 'asc'])
     .withOption('autoWidth', false)
@@ -114,22 +72,19 @@ export default function commandIndexController(
       });
     });
 
-    $scope.commands.data = commands;
-    $scope.commands.loaded = true;
-    $scope.commands.status = response['systems'].status;
-    $scope.commands.error = false;
-    $scope.commands.errorMessage = '';
+    $scope.response = response['systems'];
+    $scope.data = commands;
   };
 
   $scope.failureCallback = function(response) {
-    $scope.commands.data = [];
-    $scope.commands.loaded = false;
-    $scope.commands.error = true;
-    $scope.commands.status = response.status;
-    $scope.commands.errorMessage = response.data.message;
+    $scope.response = response;
+    $scope.data = {};
   };
 
   const loadCommands = function() {
+    $scope.response = undefined;
+    $scope.data = {};
+
     $rootScope.systemsPromise.then(
       () => {
         // We don't actually use the CommandService.getCommands(), but make the
