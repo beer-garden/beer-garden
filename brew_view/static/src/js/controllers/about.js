1f3da4a7eae14952b35453740a7741d05f61be71
Matt Patrick <hazmat345@gmail.com>
2018-08-11 16:35:13 -0400

    About page using new directive

--- brew_view/static/src/js/controllers/about.js
+++ brew_view/static/src/js/controllers/about.js
@@ -7,29 +7,13 @@ AboutController.$inject = ['$scope', 'UtilityService'];
  * @param  {Object} VersionService Beer-Garden's version service object.
  */
 export default function AboutController($scope, UtilityService) {
-  $scope.version = {
-    data: {},
-    loaded: false,
-    error: false,
-    errorMessage: '',
-  };
-
-  $scope.successCallback = function(response) {
-    $scope.version.data = response.data;
-    $scope.version.loaded = true;
-    $scope.version.error = false;
-    $scope.version.errorMessage = '';
-  },
-
-  $scope.failureCallback = function(response) {
-    $scope.version.data = {};
-    $scope.version.loaded = false;
-    $scope.version.error = true;
-    $scope.version.errorMessage = response.data.message;
-  },
-
   UtilityService.getVersion().then(
-    $scope.successCallback,
-    $scope.failureCallback
+    (response) => {
+      $scope.response = response;
+      $scope.data = response.data;
+    },
+    (response) => {
+      $scope.response = response;
+    }
   );
 };
