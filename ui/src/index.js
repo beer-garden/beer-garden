45f666b3a5d1f18724ad24539c73fad4a4e2cd8a
Logan Jones <loganasherjones@gmail.com>
2018-12-20 09:14:01 -0500

    Adding some themes just for testing.

--- ui/src/index.js
+++ ui/src/index.js
@@ -6,14 +6,25 @@ import 'typeface-roboto';
 import Root from './containers/Root';
 import configureStore from './store/configureStore';
 import * as serviceWorker from './serviceWorker';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 
 const store = configureStore();
+const defaultTheme = createMuiTheme({ typography: { useNextVariants: true } });
+const darkTheme = createMuiTheme({
+  typography: { useNextVariants: true },
+  palette: { type: 'dark' },
+});
 
 render(
   <Provider store={store}>
-    <Router>
-      <Root />
-    </Router>
+    <MuiThemeProvider theme={defaultTheme}>
+      <Router>
+        <CssBaseline>
+          <Root />
+        </CssBaseline>
+      </Router>
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
