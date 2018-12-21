c6aef41f322261b462b8e8d40d4435e597c4904b
Matt Patrick <hazmat345@gmail.com>
2018-12-21 17:52:45 -0500

    Simplifying model conversion helper function

--- test/utils.py
+++ test/utils.py
@@ -1,23 +1,24 @@
 import unittest
+
 from functools import partial
 
 import brewtils.schemas
-from bg_utils.mongo.parser import BeerGardenSchemaParser
+from bg_utils.mongo.parser import MongoParser
 from brewtils.schema_parser import SchemaParser
 from brewtils.test.comparable import assert_system_equal
 
 
-def convert(model, serializer=None, parser=None):
+def convert(model, parser=None):
     schema = getattr(brewtils.schemas, model.schema)
     return parser._do_parse(
-        serializer._do_serialize(schema(), model, True),
+        SchemaParser._do_serialize(schema(), model, True),
         schema(),
         True,
     )
 
 
-mongo2brew = partial(convert, serializer=BeerGardenSchemaParser, parser=SchemaParser)
-brew2mongo = partial(convert, serializer=SchemaParser, parser=BeerGardenSchemaParser)
+mongo2brew = partial(convert, parser=SchemaParser)
+brew2mongo = partial(convert, parser=MongoParser)
 
 
 def test_convert(bg_system):
