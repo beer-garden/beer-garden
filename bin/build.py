01ffa5e5d95cb1c7aa594e36c9916e09cb41a847
Matt Patrick <hazmat345@gmail.com>
2019-01-24 14:00:39 -0500

    Running black on bin/build.py

--- bin/build.py
+++ bin/build.py
@@ -9,21 +9,22 @@ import sys
 
 BUILD_IMAGE = "bgio/build"
 NODE_IMAGE = "node:10.9"
-SUPPORTED_DISTRIBUTIONS = ['centos6', 'centos7']
-SUPPORTED_PYTHONS = ['python2', 'python3']
-BUILD_TYPES = ['rpm']
+SUPPORTED_DISTRIBUTIONS = ["centos6", "centos7"]
+SUPPORTED_PYTHONS = ["python2", "python3"]
+BUILD_TYPES = ["rpm"]
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
-SRC_PATH = os.path.abspath(os.path.join(SCRIPT_PATH, '..'))
+SRC_PATH = os.path.abspath(os.path.join(SCRIPT_PATH, ".."))
 RPM_BUILD_SCRIPT = os.path.join("/", "src", "bin", "rpm_build.sh")
+
 
 def parse_args(cli_args):
     parser = argparse.ArgumentParser(description="Build beer-garden artifacts.")
-    parser.add_argument('type', choices=BUILD_TYPES)
-    parser.add_argument('--distribution', choices=SUPPORTED_DISTRIBUTIONS)
-    parser.add_argument('--python', choices=SUPPORTED_PYTHONS)
-    parser.add_argument('--local', action='store_true', default=False)
-    parser.add_argument('--docker-envs', type=json.loads, default='{}')
+    parser.add_argument("type", choices=BUILD_TYPES)
+    parser.add_argument("--distribution", choices=SUPPORTED_DISTRIBUTIONS)
+    parser.add_argument("--python", choices=SUPPORTED_PYTHONS)
+    parser.add_argument("--local", action="store_true", default=False)
+    parser.add_argument("--docker-envs", type=json.loads, default="{}")
     return parser.parse_args(cli_args)
 
 
@@ -39,7 +40,7 @@ def build_rpms(cli_dist, cli_python, local, docker_envs):
     else:
         build_dists = SUPPORTED_DISTRIBUTIONS
 
-    build_python = cli_python or 'python3'
+    build_python = cli_python or "python3"
     if build_python not in SUPPORTED_PYTHONS:
         print("Invalid python (%s) for RPM build" % cli_python)
         print("Supported distributions are: %s" % SUPPORTED_PYTHONS)
@@ -47,24 +48,27 @@ def build_rpms(cli_dist, cli_python, local, docker_envs):
 
     # This massages the input env dict into ["-e", "key=value"]
     # It's gross, don't worry about it
-    env_vars = list(itertools.chain.from_iterable(zip(
-        itertools.repeat('-e'),
-        [k+'='+v for k, v in docker_envs.items()]
-    )))
+    env_vars = list(
+        itertools.chain.from_iterable(
+            zip(itertools.repeat("-e"), [k + "=" + v for k, v in docker_envs.items()])
+        )
+    )
 
     if local:
-        js_cmd = [
-            "docker", "run", "--rm", "-v", SRC_PATH + ":/src"
-            ] + env_vars + [
-            NODE_IMAGE, "make", "-C", "/src/brew-view", "package-js"]
+        js_cmd = (
+            ["docker", "run", "--rm", "-v", SRC_PATH + ":/src"]
+            + env_vars
+            + [NODE_IMAGE, "make", "-C", "/src/brew-view", "package-js"]
+        )
         subprocess.call(js_cmd)
 
     for dist in build_dists:
-        tag = dist + '-' + build_python
-        cmd = [
-            "docker", "run", "--rm", "-v", SRC_PATH + ":/src"
-            ] + env_vars + [
-            BUILD_IMAGE+':'+tag, RPM_BUILD_SCRIPT, "-r", dist[-1]]
+        tag = dist + "-" + build_python
+        cmd = (
+            ["docker", "run", "--rm", "-v", SRC_PATH + ":/src"]
+            + env_vars
+            + [BUILD_IMAGE + ":" + tag, RPM_BUILD_SCRIPT, "-r", dist[-1]]
+        )
         if local:
             cmd.append("--local")
         subprocess.call(cmd)
@@ -72,7 +76,7 @@ def build_rpms(cli_dist, cli_python, local, docker_envs):
 
 def main():
     args = parse_args(sys.argv[1:])
-    if args.type == 'rpm':
+    if args.type == "rpm":
         build_rpms(args.distribution, args.python, args.local, args.docker_envs)
 
 
