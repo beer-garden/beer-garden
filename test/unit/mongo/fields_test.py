ddb2937efa6b26e85c0b5cbf770fed832bee1491
Matt Patrick <hazmat345@gmail.com>
2019-06-19 19:23:44 -0400

    Updating parser tests to pytest, using brewtils fixtures

--- test/unit/mongo/fields_test.py
+++ test/unit/mongo/fields_test.py
@@ -1,13 +1,11 @@
-import unittest
-
 from bg_utils.mongo.fields import DummyField
 
 
-class DummyFieldTest(unittest.TestCase):
+class TestDummyField(object):
     def test_to_python(self):
         field = DummyField()
-        self.assertEqual("value", field.to_python("value"))
+        assert field.to_python("value") == "value"
 
     def test_to_mongo(self):
         field = DummyField()
-        self.assertIsNone(field.to_mongo("value"))
+        assert field.to_mongo("value") is None
