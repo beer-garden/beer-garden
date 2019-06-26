5e961fa4d3034070ea18ea8c49d4ffd07006771b
Matt Patrick <hazmat345@gmail.com>
2019-06-26 12:58:29 -0400

    Moving request update logic into bartender

--- brew_view/handlers/v1/request.py
+++ brew_view/handlers/v1/request.py
@@ -15,14 +15,12 @@ from brew_view import thrift_context
 from brew_view.authorization import authenticated, Permissions
 from brew_view.base_handler import BaseHandler
 from brew_view.metrics import request_created, http_api_latency_total, request_latency
-from brew_view.metrics import request_updated
 from brewtils.errors import (
     ModelValidationError,
     RequestPublishException,
     TimeoutExceededError,
 )
 from brewtils.models import Events
-from brewtils.models import Request as BrewtilsRequest
 from brewtils.schema_parser import SchemaParser
 
 
@@ -61,6 +59,7 @@ class RequestAPI(BaseHandler):
 
         self.write(self.parser.serialize_request(req, to_string=False))
 
+    @coroutine
     @authenticated(permissions=[Permissions.REQUEST_UPDATE])
     def patch(self, request_id):
         """
@@ -103,79 +102,15 @@ class RequestAPI(BaseHandler):
         tags:
           - Requests
         """
-        req = Request.objects.get(id=request_id)
-        operations = self.parser.parse_patch(
-            self.request.decoded_body, many=True, from_string=True
-        )
-        wait_event = None
+        with thrift_context() as client:
+            try:
+                thrift_response = yield client.updateRequest(
+                    request_id, self.request.decoded_body
+                )
+            except bg_utils.bg_thrift.InvalidRequest as ex:
+                raise ModelValidationError(ex.message)
 
-        # We note the status before the operations, because it is possible for the
-        # operations to update the status of the request. In that case, because the
-        # updates are coming in in a single request it is okay to update the output or
-        # error_class. Ideally this would be handled correctly when we better integrate
-        # PatchOperations with their models.
-        status_before = req.status
-
-        for op in operations:
-            if op.operation == "replace":
-                if op.path == "/status":
-                    if op.value.upper() in BrewtilsRequest.STATUS_LIST:
-                        req.status = op.value.upper()
-
-                        if op.value.upper() == "IN_PROGRESS":
-                            self.request.event.name = Events.REQUEST_STARTED.name
-
-                        elif op.value.upper() in BrewtilsRequest.COMPLETED_STATUSES:
-                            self.request.event.name = Events.REQUEST_COMPLETED.name
-
-                            if request_id in brew_view.request_map:
-                                wait_event = brew_view.request_map[request_id]
-                    else:
-                        error_msg = "Unsupported status value '%s'" % op.value
-                        self.logger.warning(error_msg)
-                        raise ModelValidationError(error_msg)
-                elif op.path == "/output":
-                    if req.output == op.value:
-                        continue
-
-                    if status_before in Request.COMPLETED_STATUSES:
-                        raise ModelValidationError(
-                            "Cannot update output for a request "
-                            "that is already completed"
-                        )
-                    req.output = op.value
-                elif op.path == "/error_class":
-                    if req.error_class == op.value:
-                        continue
-
-                    if status_before in Request.COMPLETED_STATUSES:
-                        raise ModelValidationError(
-                            "Cannot update error_class for a "
-                            "request that is already completed"
-                        )
-                    req.error_class = op.value
-                    self.request.event.error = True
-                else:
-                    error_msg = "Unsupported path '%s'" % op.path
-                    self.logger.warning(error_msg)
-                    raise ModelValidationError(error_msg)
-            else:
-                error_msg = "Unsupported operation '%s'" % op.operation
-                self.logger.warning(error_msg)
-                raise ModelValidationError(error_msg)
-
-        req.save()
-
-        # Metrics
-        request_updated(req, status_before)
-        self._update_job_numbers(req, status_before)
-
-        if wait_event:
-            wait_event.set()
-
-        self.request.event_extras = {"request": req, "patch": operations}
-
-        self.write(self.parser.serialize_request(req, to_string=False))
+        self.write(thrift_response)
 
     def _update_job_numbers(self, request, status_before):
         if (
