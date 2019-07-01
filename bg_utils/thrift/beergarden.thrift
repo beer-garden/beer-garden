a8ea928af3ea891bbbacf0a72db3e25bca35a936
Matt Patrick <hazmat345@gmail.com>
2019-07-01 17:46:58 -0400

    Moving more instance logic to bartender

--- bg_utils/thrift/beergarden.thrift
+++ bg_utils/thrift/beergarden.thrift
@@ -26,9 +26,9 @@ exception ConflictException {
 service BartenderBackend {
 
   // Systems
-  string createSystem(1:string system) throws (1: ConflictException ex, 2:BaseException baseEx);
+  string createSystem(1: string system) throws (1:ConflictException ex, 2:BaseException baseEx);
 
-  string updateSystem(1:string systemId, 2:string updates) throws (1:BaseException baseEx);
+  string updateSystem(1: string systemId, 2:string updates) throws (1:BaseException baseEx);
 
   void reloadSystem(1: string systemId) throws (1:InvalidSystem ex, 2:BaseException baseEx);
 
@@ -44,36 +44,40 @@ service BartenderBackend {
 
   string stopInstance(1: string instanceId) throws (1:InvalidSystem ex, 2:BaseException baseEx);
 
+  string updateInstance(1: string instanceId, 2: string newStatus) throws (1:BaseException baseEx);
+
+  void removeInstance(1: string instanceId) throws (1:BaseException baseEx);
+
   void checkIn(1: string instanceId) throws (1:InvalidSystem ex, 2:BaseException baseEx);
 
 
   // Requests
   string getRequests(1: string query) throws (1:BaseException baseEx);
 
-  string processRequest(1:string id) throws (1:InvalidRequest ex, 2: PublishException pubEx,
+  string processRequest(1: string id) throws (1:InvalidRequest ex, 2: PublishException pubEx,
     3:BaseException baseEx);
 
-   string updateRequest(1:string requestId, 2:string patch) throws (1: BaseException baseEx);
+   string updateRequest(1: string requestId, 2: string patch) throws (1: BaseException baseEx);
 
 
   // Queues
-  i32 getQueueMessageCount(1:string queueName)
+  i32 getQueueMessageCount(1: string queueName)
     throws (1:BaseException baseEx, 2:InvalidSystem invalidEx);
 
   string getAllQueueInfo() throws (1:BaseException baseEx);
 
-  void clearQueue(1:string queueName) throws (1:BaseException baseEx, 2:InvalidSystem invalidEx);
+  void clearQueue(1: string queueName) throws (1:BaseException baseEx, 2:InvalidSystem invalidEx);
 
   void clearAllQueues() throws (1:BaseException baseEx);
 
   // Jobs
-  string createJob(1:string job) throws (1:BaseException baseEx);
+  string createJob(1: string job) throws (1:BaseException baseEx);
 
-  string pauseJob(1:string jobName) throws (1:BaseException baseEx);
+  string pauseJob(1: string jobName) throws (1:BaseException baseEx);
 
-  string resumeJob(1:string jobName) throws (1:BaseException baseEx);
+  string resumeJob(1: string jobName) throws (1:BaseException baseEx);
 
-  void removeJob(1:string jobName) throws (1:BaseException baseEx);
+  void removeJob(1: string jobName) throws (1:BaseException baseEx);
 
 
   // Misc
