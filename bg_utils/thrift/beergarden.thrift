97cf8fd9bbae0f162804efeb4d028835d0f07686
Matt Patrick <hazmat345@gmail.com>
2019-07-12 13:42:54 -0400

    Adding namespaces to thrift interface

--- bg_utils/thrift/beergarden.thrift
+++ bg_utils/thrift/beergarden.thrift
@@ -26,90 +26,191 @@ exception ConflictException {
 service BartenderBackend {
 
   // Systems
-  string getSystem(1: string systemId, 2: bool includeCommands) throws (1:BaseException baseEx);
+    string getSystem(
+        1: string nameSpace,
+        2: string systemId,
+        3: bool includeCommands,
+    ) throws (1:BaseException baseEx);
 
-  string querySystems(
-      1: map<string, string> filterParams,
-      2: string orderBy,
-      3: set<string> includeFields,
-      4: set<string> excludeFields,
-      5: bool dereferenceNested
-  ) throws (1:BaseException baseEx);
+    string querySystems(
+        1: string nameSpace,
+        2: map<string, string> filterParams,
+        3: string orderBy,
+        4: set<string> includeFields,
+        5: set<string> excludeFields,
+        6: bool dereferenceNested,
+    ) throws (1:BaseException baseEx);
 
-  string createSystem(1: string system) throws (1:ConflictException ex, 2:BaseException baseEx);
+    string createSystem(
+        1: string nameSpace,
+        2: string system,
+    ) throws (1:ConflictException ex, 2:BaseException baseEx);
 
-  string updateSystem(1: string systemId, 2:string updates) throws (1:BaseException baseEx);
+    string updateSystem(
+        1: string nameSpace,
+        2: string systemId,
+        3:string updates,
+    ) throws (1:BaseException baseEx);
 
-  void reloadSystem(1: string systemId) throws (1:InvalidSystem ex, 2:BaseException baseEx);
+    void reloadSystem(
+        1: string nameSpace,
+        2: string systemId,
+    ) throws (1:InvalidSystem ex, 2:BaseException baseEx);
 
-  void removeSystem(1: string systemId) throws (1:InvalidSystem ex, 2:BaseException baseEx);
+    void removeSystem(
+        1: string nameSpace,
+        2: string systemId,
+    ) throws (1:InvalidSystem ex, 2:BaseException baseEx);
 
-  void rescanSystemDirectory() throws (1:BaseException baseEx);
-
-
-  // Instances
-  string getInstance(1: string instanceId) throws (1:BaseException baseEx);
-
-  string initializeInstance(1: string instanceId) throws (1:InvalidSystem ex, 2:BaseException baseEx);
-
-  string updateInstance(1: string instanceId, 2: string patch) throws (1:BaseException baseEx);
-
-  string startInstance(1: string instanceId) throws (1:InvalidSystem ex, 2:BaseException baseEx);
-
-  string stopInstance(1: string instanceId) throws (1:InvalidSystem ex, 2:BaseException baseEx);
-
-  string updateInstanceStatus(1: string instanceId, 2: string newStatus) throws (1:BaseException baseEx);
-
-  void removeInstance(1: string instanceId) throws (1:BaseException baseEx);
-
-  void checkIn(1: string instanceId) throws (1:InvalidSystem ex, 2:BaseException baseEx);
-
-
-  // Requests
-  string getRequest(1: string requestId) throws (1:BaseException baseEx);
-
-  string getRequests(1: string query) throws (1:BaseException baseEx);
-
-  string processRequest(1: string request, 2: double wait_timeout) throws (
-      1:InvalidRequest ex, 2:PublishException pubEx, 3:BaseException baseEx
-  );
-
-   string updateRequest(1: string requestId, 2: string patch) throws (1: BaseException baseEx);
+    void rescanSystemDirectory(
+        1: string nameSpace,
+    ) throws (1:BaseException baseEx);
 
 
-  // Queues
-  i32 getQueueMessageCount(1: string queueName)
-    throws (1:BaseException baseEx, 2:InvalidSystem invalidEx);
+    // Instances
+    string getInstance(
+        1: string nameSpace,
+        2: string instanceId,
+    ) throws (1:BaseException baseEx);
 
-  string getAllQueueInfo() throws (1:BaseException baseEx);
+    string initializeInstance(
+        1: string nameSpace,
+        2: string instanceId
+    ) throws (1:InvalidSystem ex, 2:BaseException baseEx);
 
-  void clearQueue(1: string queueName) throws (1:BaseException baseEx, 2:InvalidSystem invalidEx);
+    string updateInstance(
+        1: string nameSpace,
+        2: string instanceId,
+        3: string patch,
+    ) throws (1:BaseException baseEx);
 
-  void clearAllQueues() throws (1:BaseException baseEx);
+    string startInstance(
+        1: string nameSpace,
+        2: string instanceId,
+    ) throws (1:InvalidSystem ex, 2:BaseException baseEx);
 
-  // Jobs
-  string getJob(1: string jobId) throws (1:BaseException baseEx);
+    string stopInstance(
+        1: string nameSpace,
+        2: string instanceId,
+    ) throws (1:InvalidSystem ex, 2:BaseException baseEx);
 
-  string getJobs(1: map<string, string> filterParams) throws (1:BaseException baseEx);
+    string updateInstanceStatus(
+        1: string nameSpace,
+        2: string instanceId,
+        3: string newStatus,
+    ) throws (1:BaseException baseEx);
 
-  string createJob(1: string job) throws (1:BaseException baseEx);
+    void removeInstance(
+        1: string nameSpace,
+        2: string instanceId,
+    ) throws (1:BaseException baseEx);
 
-  string pauseJob(1: string jobName) throws (1:BaseException baseEx);
+    void checkIn(
+        1: string nameSpace,
+        2: string instanceId,
+    ) throws (1:InvalidSystem ex, 2:BaseException baseEx);
 
-  string resumeJob(1: string jobName) throws (1:BaseException baseEx);
 
-  void removeJob(1: string jobName) throws (1:BaseException baseEx);
+    // Requests
+    string getRequest(
+        1: string nameSpace,
+        2: string requestId,
+    ) throws (1:BaseException baseEx);
 
-  // Commands
-  string getCommand(1: string commandId) throws (1:BaseException baseEx);
+    string getRequests(
+        1: string nameSpace,
+        2: string query,
+    ) throws (1:BaseException baseEx);
 
-  string getCommands() throws (1:BaseException baseEx);
+    string processRequest(
+        1: string nameSpace,
+        2: string request,
+        3: double wait_timeout,
+    ) throws (1:InvalidRequest ex, 2:PublishException pubEx, 3:BaseException baseEx);
 
-  // Logging
-  string getPluginLogConfig(1: string systemName) throws (1:BaseException baseEx);
+    string updateRequest(
+        1: string nameSpace,
+        2: string requestId,
+        3: string patch,
+    ) throws (1: BaseException baseEx);
 
-  string reloadPluginLogConfig() throws (1:BaseException baseEx);
 
-  // Misc
-  string getVersion();
+    // Queues
+    i32 getQueueMessageCount(
+        1: string nameSpace,
+        2: string queueName,
+    ) throws (1:BaseException baseEx, 2:InvalidSystem invalidEx);
+
+    string getAllQueueInfo(
+        1: string nameSpace,
+    ) throws (1:BaseException baseEx);
+
+    void clearQueue(
+        1: string nameSpace,
+        2: string queueName,
+    ) throws (1:BaseException baseEx, 2:InvalidSystem invalidEx);
+
+    void clearAllQueues(
+        1: string nameSpace
+    ) throws (1:BaseException baseEx);
+
+
+    // Jobs
+    string getJob(
+        1: string nameSpace,
+        2: string jobId,
+    ) throws (1:BaseException baseEx);
+
+    string getJobs(
+        1: string nameSpace,
+        2: map<string, string> filterParams,
+    ) throws (1:BaseException baseEx);
+
+    string createJob(
+        1: string nameSpace,
+        2: string job,
+    ) throws (1:BaseException baseEx);
+
+    string pauseJob(
+        1: string nameSpace,
+        2: string jobName,
+    ) throws (1:BaseException baseEx);
+
+    string resumeJob(
+        1: string nameSpace,
+        2: string jobName,
+    ) throws (1:BaseException baseEx);
+
+    void removeJob(
+        1: string nameSpace,
+        2: string jobName,
+    ) throws (1:BaseException baseEx);
+
+
+    // Commands
+    string getCommand(
+        1: string nameSpace,
+        2: string commandId,
+    ) throws (1:BaseException baseEx);
+
+    string getCommands(
+        1: string nameSpace,
+    ) throws (1:BaseException baseEx);
+
+
+    // Logging
+    string getPluginLogConfig(
+        1: string nameSpace,
+        2: string systemName,
+    ) throws (1:BaseException baseEx);
+
+    string reloadPluginLogConfig(
+        1: string nameSpace,
+    ) throws (1:BaseException baseEx);
+
+
+    // Misc
+    string getVersion(
+        1: string nameSpace,
+    );
 }
