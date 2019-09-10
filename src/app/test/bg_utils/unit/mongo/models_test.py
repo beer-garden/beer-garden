d7cf7fc6a71ae112a5d6a4670a00244651b7cfd4
Matt Patrick <hazmat345@gmail.com>
2019-09-10 15:10:08 -0400

    Moving old model "integration" tests

--- src/app/test/bg_utils/unit/mongo/models_test.py
+++ src/app/test/bg_utils/unit/mongo/models_test.py
@@ -361,6 +361,36 @@ class TestSystem(object):
     #     self.assertEqual(
     #         new_command.description, self.default_system.commands[0].description
     #     )
+    #
+    # TODO - Same for these. They were actually in the integration test package
+    # @patch("mongoengine.queryset.QuerySet.filter", Mock(return_value=[1]))
+    # def find_unique_system(self):
+    #     system = System.find_unique("foo", "0.0.0")
+    #     assert system == 1
+    #
+    # @patch("mongoengine.queryset.QuerySet.get", Mock(return_value=[]))
+    # def find_unique_system_none(self):
+    #     system = System.find_unique("foo", "1.0.0")
+    #     assert system is None
+    #
+    # def test_system_non_unique_name(self):
+    #     s = System(name='foo', control_system='bar', instance_names=['only'])
+    #     System.ensure_indexes()
+    #     s.save()
+    #     s2 = System(name='foo', control_system='bar', instance_names=['only'])
+    #     self.assertRaises(mongoengine.NotUniqueError, s2.save)
+    #
+    # def test_system_non_unique_instance_names(self):
+    #     s = System(name='foo', control_system='bar',
+    #                instance_names=['oops', 'oops'])
+    #     System.ensure_indexes()
+    #     self.assertRaises(mongoengine.NotUniqueError, s.save)
+    #
+    # def test_default_instance_name(self):
+    #     # s = System(name='foo')
+    #     System.ensure_indexes()
+    #     self.default_system.save()
+    #     assert self.default_system.instances == []
 
 
 class TestJob(object):
