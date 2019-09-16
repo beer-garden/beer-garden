63bc91071138c6c24b6ea5188e959e5e46960d54
Matt Patrick <hazmat345@gmail.com>
2019-09-16 17:16:31 -0400

    ThriftClient async calls now just use an executor directly

--- src/app/beer_garden/api/thrift/client.py
+++ src/app/beer_garden/api/thrift/client.py
@@ -1,8 +1,25 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+
 import brewtils.thrift
-from thriftpy2.rpc import make_client, make_aio_client
+from thriftpy2.rpc import make_client
+
+pool = ThreadPoolExecutor(50)
 
 
-class ThriftClient:
+class FakeClient(object):
+    def __getattr__(self, _api):
+        return partial(self, _api)
+
+    def __call__(self, *args, **kwargs):
+        from beer_garden.api.thrift.handler import BartenderHandler
+        return asyncio.get_event_loop().run_in_executor(
+            pool, getattr(BartenderHandler, args[0]), *args[1:]
+        )
+
+
+class ThriftClient(object):
     # These hard coded values should not exist after the refactor.
     def __init__(self, host="0.0.0.0", port=9090):
         self._host = host
@@ -21,13 +38,15 @@ class ThriftClient:
         self._client.close()
 
     async def __aenter__(self):
-        self._client = await make_aio_client(
-            brewtils.thrift.bg_thrift.BartenderBackend,
-            host=self._host,
-            port=self._port,
-            socket_timeout=13000,
-        )
-        return self._client
+        # self._client = await make_aio_client(
+        #     brewtils.thrift.bg_thrift.BartenderBackend,
+        #     host=self._host,
+        #     port=self._port,
+        #     socket_timeout=13000,
+        # )
+        # return self._client
+        return FakeClient()
 
     async def __aexit__(self, exc_type, exc, tb):
-        self._client.close()
+        # self._client.close()
+        pass
