26a6a9ea8789db2ffa29ea3861c0ef04adc013eb
Matt Patrick <hazmat345@gmail.com>
2019-09-27 20:48:22 -0400

    Modifying the executor to pass kwargs

--- src/app/beer_garden/api/http/client.py
+++ src/app/beer_garden/api/http/client.py
@@ -1,19 +1,21 @@
+# -*- coding: utf-8 -*-
 import asyncio
 from concurrent.futures.thread import ThreadPoolExecutor
 from functools import partial
 
-pool = ThreadPoolExecutor(50)
+import beer_garden.api
 
 
 class ExecutorClient(object):
+
+    pool = ThreadPoolExecutor(50)
+
     def __getattr__(self, _api):
         return partial(self, _api)
 
     def __call__(self, *args, **kwargs):
-        from beer_garden.api.thrift.handler import BartenderHandler
-
         return asyncio.get_event_loop().run_in_executor(
-            pool, getattr(BartenderHandler, args[0]), *args[1:]
+            self.pool, partial(getattr(beer_garden.api, args[0]), *args[1:], **kwargs)
         )
 
     async def __aenter__(self):
@@ -21,3 +23,6 @@ class ExecutorClient(object):
 
     async def __aexit__(self, exc_type, exc, tb):
         pass
+
+
+client = ExecutorClient()
