be2726de8bc1521ec15dcc0876d413a8c58005db
Matt Patrick <hazmat345@gmail.com>
2019-10-07 10:54:42 -0400

    Simplifying client serialize method

--- src/app/beer_garden/api/http/client.py
+++ src/app/beer_garden/api/http/client.py
@@ -6,21 +6,6 @@ from concurrent.futures.thread import ThreadPoolExecutor
 from functools import partial
 
 import six
-from brewtils.models import (
-    System,
-    Instance,
-    Command,
-    Parameter,
-    Request,
-    PatchOperation,
-    LoggingConfig,
-    Event,
-    Queue,
-    Principal,
-    Role,
-    RefreshToken,
-    Job,
-)
 from brewtils.schema_parser import SchemaParser
 
 import beer_garden.api
@@ -57,36 +42,6 @@ class ExecutorClient(object):
         """
         if isinstance(model, (six.string_types, dict)):
             return model
-        elif isinstance(model, System):
-            return cls.parser.serialize_system(model, to_string=to_string, **kwargs)
-        elif isinstance(model, Instance):
-            return cls.parser.serialize_instance(model, to_string=to_string, **kwargs)
-        elif isinstance(model, Command):
-            return cls.parser.serialize_command(model, to_string=to_string, **kwargs)
-        elif isinstance(model, Parameter):
-            return cls.parser.serialize_parameter(model, to_string=to_string, **kwargs)
-        elif isinstance(model, Request):
-            return cls.parser.serialize_request(model, to_string=to_string, **kwargs)
-        elif isinstance(model, PatchOperation):
-            return cls.parser.serialize_patch(model, to_string=to_string, **kwargs)
-        elif isinstance(model, LoggingConfig):
-            return cls.parser.serialize_logging_config(
-                model, to_string=to_string, **kwargs
-            )
-        elif isinstance(model, Event):
-            return cls.parser.serialize_event(model, to_string=to_string, **kwargs)
-        elif isinstance(model, Queue):
-            return cls.parser.serialize_queue(model, to_string=to_string, **kwargs)
-        elif isinstance(model, Principal):
-            return cls.parser.serialize_principal(model, to_string=to_string, **kwargs)
-        elif isinstance(model, Role):
-            return cls.parser.serialize_role(model, to_string=to_string, **kwargs)
-        elif isinstance(model, RefreshToken):
-            return cls.parser.serialize_refresh_token(
-                model, to_string=to_string, **kwargs
-            )
-        elif isinstance(model, Job):
-            return cls.parser.serialize_job(model, to_string=to_string, **kwargs)
         elif isinstance(model, list):
             nested_kwargs = copy.copy(kwargs)
             nested_kwargs["to_string"] = False
@@ -96,4 +51,4 @@ class ExecutorClient(object):
 
             return json.dumps(serialized) if to_string else serialized
 
-        raise ValueError("Unable to determine model type")
+        return cls.parser.serialize(model, to_string=to_string, **kwargs)
