069bdc71c3d24a022487183829ba0664a669fb08
Matt Patrick <hazmat345@gmail.com>
2019-10-11 13:02:58 -0400

    Commands module now converts to brewtils models

--- src/app/beer_garden/commands.py
+++ src/app/beer_garden/commands.py
@@ -1,10 +1,41 @@
 # -*- coding: utf-8 -*-
+from typing import List
+
+import brewtils.models
+from brewtils.schema_parser import SchemaParser
+
 from beer_garden.db.mongo.models import Command
 
 
-def get_command(command_id):
-    return Command.objects.get(id=command_id)
+def get_command(command_id: str) -> brewtils.models.Command:
+    """Retrieve an individual Command
+
+    Args:
+        command_id: The Command ID
+
+    Returns:
+        The Command
+
+    """
+    return SchemaParser.parse_command(
+        SchemaParser.serialize_command(
+            Command.objects.get(id=command_id), to_string=False
+        ),
+        from_string=False,
+    )
 
 
-def get_commands():
-    return Command.objects.all()
+def get_commands() -> List[brewtils.models.Command]:
+    """Retrieve all Commands
+
+    Returns:
+        The Commands
+
+    """
+    return SchemaParser.parse_command(
+        SchemaParser.serialize_command(
+            Command.objects.all(), to_string=False, many=True
+        ),
+        from_string=False,
+        many=True,
+    )
