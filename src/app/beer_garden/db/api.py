3d0e8593f0c320be64f7234a722cd308b3bd4947
Matt Patrick <hazmat345@gmail.com>
2019-10-18 19:22:19 -0400

    Adding simple db subsystem api

---
+++ src/app/beer_garden/db/api.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import beer_garden.db.mongo.api
+
+query = beer_garden.db.mongo.api.query
+query_unique = beer_garden.db.mongo.api.query_unique
+create = beer_garden.db.mongo.api.create
+update = beer_garden.db.mongo.api.update
+delete = beer_garden.db.mongo.api.delete
+reload = beer_garden.db.mongo.api.reload
