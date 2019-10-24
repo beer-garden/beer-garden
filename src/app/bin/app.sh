9bcc8c7a5cdb3239cdda3af4cccba0e4b3b775be
Matt Patrick <hazmat345@gmail.com>
2019-10-24 13:12:58 -0400

    Fixing startup scripts

--- src/app/bin/app.sh
+++ src/app/bin/app.sh
@@ -3,7 +3,8 @@
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 APP_ROOT="$(dirname "$SCRIPT_DIRECTORY")"
 
-CONFIG_FILE="$APP_ROOT/dev_conf/config.yaml"
+CONFIG_FILE="$APP_ROOT/example_configs/config.yaml"
+LOG_CONFIG_FILE="$APP_ROOT/example_configs/logging-config.yaml"
 
-CMD="beer_garden -c $CONFIG_FILE"
+CMD="beer_garden -c $CONFIG_FILE -l $LOG_CONFIG_FILE"
 python -m $CMD
