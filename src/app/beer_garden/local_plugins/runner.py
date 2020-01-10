d9f263db4abfe609dfe5e7c4872ea13e411ca659
Matt Patrick <hazmat345@gmail.com>
2020-01-10 12:23:56 -0500

    Using directory instead of random string for runner logger name

--- src/app/beer_garden/local_plugins/runner.py
+++ src/app/beer_garden/local_plugins/runner.py
@@ -23,30 +23,30 @@ class ProcessRunner(Thread):
         process_cwd: Path,
         process_env: dict,
     ):
-        self.logger = logging.getLogger(__name__)
-        self.process = None
-
         self.runner_id = runner_id
         self.process_args = process_args
         self.process_cwd = process_cwd
         self.process_env = process_env
+
+        self.process = None
+        self.runner_name = process_cwd.name
+        self.logger = logging.getLogger(f"{__name__}.{self.runner_name}")
 
         Thread.__init__(self, name=self.runner_id)
 
     def kill(self):
         """Kill the underlying plugin process with SIGKILL"""
         if self.process and self.process.poll() is None:
-            self.logger.warning(f"About to kill process {self.runner_id}")
+            self.logger.warning(f"About to kill process")
             self.process.kill()
 
     def run(self):
-        """Runs the plugin
+        """Runs the plugin process
 
         Run the plugin using the entry point specified with the generated environment in
-        its own subprocess. Pipes STDOUT and STDERR such that when the plugin stops
-        executing (or IO is flushed) it will log it.
+        its own subprocess.
         """
-        self.logger.info(f"Starting runner {self.runner_id}: {self.process_args}")
+        self.logger.info(f"Starting process with args {self.process_args}")
 
         try:
             self.process = subprocess.Popen(
@@ -61,7 +61,7 @@ class ProcessRunner(Thread):
             while self.process.poll() is None:
                 sleep(0.1)
 
-            self.logger.info(f"Plugin {self.runner_id} is officially stopped")
+            self.logger.info(f"Plugin is officially stopped")
 
         except Exception as ex:
-            self.logger.exception(f"Plugin {self.runner_id} died: {ex}")
+            self.logger.exception(f"Plugin died: {ex}")
