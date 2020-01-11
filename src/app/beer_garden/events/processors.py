1c359c5ccc940f040822bfc767893046e55478a7
Matt Patrick <hazmat345@gmail.com>
2020-01-10 20:58:15 -0500

    Adding RequeueProcessor and CallableProcessor

--- src/app/beer_garden/events/processors.py
+++ src/app/beer_garden/events/processors.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from typing import Callable
+
+from brewtils.models import Event
 from brewtils.schema_parser import SchemaParser
 
 from beer_garden.events.events_manager import EventProcessor
@@ -13,3 +16,25 @@ class PrintProcessor(EventProcessor):
 
     def process_next_message(self, event):
         print(SchemaParser.serialize(event), file=self._stream)
+
+
+class RequeueProcessor(EventProcessor):
+    """Processor that simply forwards events to another Queue"""
+
+    def __init__(self, queue):
+        super().__init__()
+        self._queue = queue
+
+    def process_next_message(self, event):
+        self._queue.put(event)
+
+
+class CallableProcessor(EventProcessor):
+    """Processor that will invoke a given Callable with the event"""
+
+    def __init__(self, callable_obj: Callable[[Event], None]):
+        super().__init__()
+        self._callable = callable_obj
+
+    def process_next_message(self, event):
+        self._callable(event)
