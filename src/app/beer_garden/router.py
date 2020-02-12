7723b0bf75c6629bff3ccaaa8416fa255398662a
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2020-02-12 15:54:16 -0500

    Fixed some issues

--- src/app/beer_garden/router.py
+++ src/app/beer_garden/router.py
@@ -55,17 +55,14 @@ def forward_elgible_request(forward: brewtils.models.Forward):
     :param forward: Forward request object that needs to be evaluated
     :return: Boolean response on if the object should be forwarded
     """
-    if forward.target_garden_name is None:
-        for arg in forward.args:
-            if arg.schema in [
-                brewtils.models.Request.schema,
-                brewtils.models.RequestTemplate.schema,
-            ]:
-                forward.target_garden_name = get_system_mapping(
-                    name_space=arg.namespace,
-                    system=arg.system,
-                    version=arg.system_version,
-                )
+    # if forward.target_garden_name is None:
+    #     for arg in forward.args:
+    #         if isinstance(arg, (brewtils.models.Request, brewtils.models.RequestTemplate)):
+    #             forward.target_garden_name = get_system_mapping(
+    #                 name_space=arg.namespace,
+    #                 system=arg.system,
+    #                 version=arg.system_version,
+    #             )
     return forward_elgible(forward)
 
 
@@ -77,21 +74,22 @@ def forward_elgible_instance(forward: brewtils.models.Forward):
     """
     if forward.target_garden_name is None:
         for arg in forward.args:
-            if arg.schema in [brewtils.models.Instance.schema]:
-                bg_systems = beer_garden.systems.get_systems(instances__contains=arg)
-                if len(bg_systems) == 1:
-                    forward.target_garden_name = System_Garden_Mapping.get(
-                        str(bg_systems[0]), None
-                    )
-            elif isinstance(arg, str):
+
+            if isinstance(arg, str):
                 instance = beer_garden.instances.get_instance(arg)
                 bg_systems = beer_garden.systems.get_systems(
                     instances__contains=instance
                 )
-                if len(bg_systems) == 1:
-                    forward.target_garden_name = System_Garden_Mapping.get(
-                        str(bg_systems[0]), None
-                    )
+                # if len(bg_systems) == 1:
+                #     forward.target_garden_name = System_Garden_Mapping.get(
+                #         str(bg_systems[0]), None
+                #     )
+            elif isinstance(arg, brewtils.models.Instance):
+                bg_systems = beer_garden.systems.get_systems(instances__contains=arg)
+                # if len(bg_systems) == 1:
+                #     forward.target_garden_name = System_Garden_Mapping.get(
+                #         str(bg_systems[0]), None
+                #     )
 
     return forward_elgible(forward)
 
@@ -104,69 +102,67 @@ def forward_elgible_system(forward: brewtils.models.Forward):
     """
     if forward.target_garden_name is None:
         for arg in forward.args:
-            if arg.schema in [brewtils.models.System]:
-                forward.target_garden_name = System_Garden_Mapping.get(str(arg, None))
-            elif isinstance(arg, str):
+
+            if isinstance(arg, str):
                 system = beer_garden.systems.get_system(arg)
-                forward.target_garden_name = System_Garden_Mapping.get(
-                    str(system, None)
-                )
+                #forward.target_garden_name = System_Garden_Mapping.get(
+                #    str(system, None)
+                #)
+            elif isinstance(arg, brewtils.models.System):
+                forward.target_garden_name = System_Garden_Mapping.get(str(arg, None))
 
     return forward_elgible(forward)
 
 
 class Route_Class(Enum):
     # How to create new Route Class
-    # ARG 1 = Target Module
-    # ARG 2 = Target Function
-    # ARG 3 = Routing Eligibility Check
+    # ARG 1 = Target Function
+    # ARG 2 = Routing Eligibility Check
 
-    REQUEST_CREATE = (beer_garden.requests, "process_request", forward_elgible_request)
-    REQUEST_UPDATE = (beer_garden.requests, "update_request", None)
-    REQUEST_READ = (beer_garden.requests, "get_request", None)
-    REQUEST_READ_ALL = (beer_garden.requests, "get_requests", None)
+    REQUEST_CREATE = (beer_garden.requests.process_request, forward_elgible_request)
+    REQUEST_UPDATE = (beer_garden.requests.update_request, None)
+    REQUEST_READ = (beer_garden.requests.get_request, None)
+    REQUEST_READ_ALL = (beer_garden.requests.get_requests, None)
 
-    COMMAND_READ = (beer_garden.commands, "get_command", None)
-    COMMAND_READ_ALL = (beer_garden.commands, "get_commands", None)
+    COMMAND_READ = (beer_garden.commands.get_command, None)
+    COMMAND_READ_ALL = (beer_garden.commands.get_commands, None)
 
-    INSTANCE_READ = (beer_garden.instances, "get_instance", None)
+    INSTANCE_READ = (beer_garden.instances.get_instance, None)
     INSTANCE_DELETE = (
-        beer_garden.instances,
-        "remove_instance",
+        beer_garden.instances.remove_instance,
         forward_elgible_instance,
     )
-    INSTANCE_UPDATE = (beer_garden.plugin, "update", forward_elgible_instance)
-    INSTANCE_INITIALIZE = (beer_garden.plugin, "initialize", None)
-    INSTANCE_START = (beer_garden.plugin, "start", None)
+    INSTANCE_UPDATE = (beer_garden.plugin.update, forward_elgible_instance)
+    INSTANCE_INITIALIZE = (beer_garden.plugin.initialize, None)
+    INSTANCE_START = (beer_garden.plugin.start, None)
 
-    JOB_CREATE = (beer_garden.scheduler, "create_job", None)
-    JOB_READ = (beer_garden.scheduler, "get_job", None)
-    JOB_READ_ALL = (beer_garden.scheduler, "get_jobs", None)
-    JOB_PAUSE = (beer_garden.scheduler, "pause_job", None)
-    JOB_RESUME = (beer_garden.scheduler, "resume_job", None)
-    JOB_DELETE = (beer_garden.scheduler, "remove_job", None)
+    JOB_CREATE = (beer_garden.scheduler.create_job, None)
+    JOB_READ = (beer_garden.scheduler.get_job, None)
+    JOB_READ_ALL = (beer_garden.scheduler.get_jobs, None)
+    JOB_PAUSE = (beer_garden.scheduler.pause_job, None)
+    JOB_RESUME = (beer_garden.scheduler.resume_job, None)
+    JOB_DELETE = (beer_garden.scheduler.remove_job, None)
 
-    SYSTEM_CREATE = (beer_garden.systems, "create_system", None)
-    SYSTEM_READ = (beer_garden.systems, "get_system", None)
-    SYSTEM_READ_ALL = (beer_garden.systems, "get_systems", None)
-    SYSTEM_UPDATE = (beer_garden.systems, "update_system", forward_elgible_system)
-    SYSTEM_RESCAN = (beer_garden.systems, "rescan_system_directory", None)
-    SYSTEM_DELETE = (beer_garden.systems, "remove_system", forward_elgible_system)
+    SYSTEM_CREATE = (beer_garden.systems.create_system, None)
+    SYSTEM_READ = (beer_garden.systems.get_system, None)
+    SYSTEM_READ_ALL = (beer_garden.systems.get_systems, None)
+    SYSTEM_UPDATE = (beer_garden.systems.update_system, forward_elgible_system)
+    SYSTEM_RESCAN = (beer_garden.systems.rescan_system_directory, None)
+    SYSTEM_DELETE = (beer_garden.systems.remove_system, forward_elgible_system)
 
-    GARDEN_CREATE = (beer_garden.garden, "create_garden", None)
-    GARDEN_READ = (beer_garden.garden, "get_garden", None)
-    GARDEN_UPDATE = (beer_garden.garden, "update_garden", None)
-    GARDEN_DELETE = (beer_garden.garden, "remove_garden", None)
+    GARDEN_CREATE = (beer_garden.garden.create_garden, None)
+    GARDEN_READ = (beer_garden.garden.get_garden, None)
+    GARDEN_UPDATE = (beer_garden.garden.update_garden, None)
+    GARDEN_DELETE = (beer_garden.garden.remove_garden, None)
 
-    LOG_READ = (beer_garden.log, "get_plugin_log_config", None)
-    LOG_RELOAD = (beer_garden.log, "reload_plugin_log_config", None)
+    LOG_READ = (beer_garden.log.get_plugin_log_config, None)
+    LOG_RELOAD = (beer_garden.log.reload_plugin_log_config, None)
 
-    QUEUE_READ = (beer_garden.queues, "get_all_queue_info", None)
-    QUEUE_DELETE = (beer_garden.queues, "clear_queue", None)
-    QUEUE_DELETE_ALL = (beer_garden.queues, "clear_all_queues", None)
+    QUEUE_READ = (beer_garden.queues.get_all_queue_info, None)
+    QUEUE_DELETE = (beer_garden.queues.clear_queue, None)
+    QUEUE_DELETE_ALL = (beer_garden.queues.clear_all_queues, None)
 
-    def __init__(self, module, function, forward_eligible):
-        self.module = module
+    def __init__(self, function, forward_eligible):
         self.function = function
         self.forward_eligible = forward_eligible
 
@@ -175,7 +171,10 @@ class Route_Class(Enum):
             forward.source_garden_name = _local_garden()
         if self.forward_eligible and self.forward_eligible(forward):
             return forward_routing(forward)
-        return partial(self.module, self.function, *forward.args, **forward.kwargs)
+        #Todo: Figure out why this doesn't work
+        #return partial(self.module, self.function, *forward.args, **forward.kwargs)
+
+        return partial(self.value[0], *forward.args, **forward.kwargs)()
 
 
 System_Garden_Mapping = dict()
