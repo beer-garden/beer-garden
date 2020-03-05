c8a8282f7c8e2e1a3d0d2bf74915a6c821794130
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2020-03-05 14:37:06 -0500

    Replicating the Systems page for Gardens

--- src/ui/src/js/controllers/admin_garden.js
+++ src/ui/src/js/controllers/admin_garden.js
@@ -35,6 +35,10 @@ export default function adminGardenController(
     );
   };
 
+  $scope.editGarden = function(garden) {
+
+  };
+
   let loadAll = function() {
     $scope.response = undefined;
     $scope.data = [];
