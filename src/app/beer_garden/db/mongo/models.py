4322789df4e3e60323dbd81cdb72f8fa3f927d93
west270 <bjwest31@outlook.com>
2020-03-12 13:45:31 -0400

    Added show hidden checkbox and hidden to models

--- src/app/beer_garden/db/mongo/models.py
+++ src/app/beer_garden/db/mongo/models.py
@@ -203,6 +203,7 @@ class Command(MongoModel, Document):
     schema = DictField()
     form = DictField()
     template = StringField()
+    hidden = BooleanField()
     icon_name = StringField()
     # reverse_delete_rule=CASCADE is set later, can't set until System is defined
     system = ReferenceField("System")
--- src/ui/src/js/controllers/command_index.js
+++ src/ui/src/js/controllers/command_index.js
@@ -2,43 +2,93 @@
 commandIndexController.$inject = [
   '$rootScope',
   '$scope',
+  '$q',
+  'SystemService',
+  'CommandService',
   'DTOptionsBuilder',
+  'detailSystems',
+  'commands',
 ];
 
 /**
  * commandIndexController - Angular controller for all commands page.
  * @param  {$rootScope} $rootScope   Angular's $rootScope object.
  * @param  {$scope} $scope           Angular's $scope object.
+ * @param  {$q} $q                   Angular's $q object.
+ * @param  {Object} SystemService    Beer-Garden system service.
+ * @param  {Object} CommandService   Beer-Garden command service.
  * @param  {Object} DTOptionsBuilder Data-tables' builder for options.
  */
 export default function commandIndexController(
-    $rootScope,
-    $scope,
-    DTOptionsBuilder) {
+  $rootScope,
+  $scope,
+  $q,
+  SystemService,
+  CommandService,
+  DTOptionsBuilder,
+  detailSystems,
+  commands) {
   $scope.setWindowTitle('commands');
-
+  $scope.filterHidden = false;
   $scope.dtOptions = DTOptionsBuilder.newOptions()
-    .withOption('order', [[0, 'asc'], [1, 'asc'], [2, 'asc'], [3, 'asc']])
+    .withOption('order', [4, 'asc'])
     .withOption('autoWidth', false)
+    //.withOption('hiddenContainer', true)
     .withBootstrap();
 
+  $scope.hiddenComparator = function(expected, actual){
+    return actual || !expected;
+  };
+
+
+
+  $scope.stateParams = function(entry) {
+    return {
+      id: entry.id,
+      name: entry.name,
+      systemName: entry.system,
+      systemVersion: $rootScope.getVersionForUrl(entry.version),
+    };
+  };
+
   $scope.successCallback = function(response) {
-    // Pull out what we care about
+  // $scope.successCallback = function(systems) {
+    // let systems = response['systems'].data;
+    let systems = response.data;
     let commands = [];
 
-    response.data.forEach((system) => {
+    // Sort the systems
+    systems.sort((a, b) => {
+      let aName = a.display_name || a.name;
+      let bName = b.display_name || b.name;
+
+      if (aName < bName) return -1;
+      if (aName > bName) return 1;
+      return 0;
+    });
+
+    systems.forEach((system) => {
+      // Sort the commands
+      system.commands.sort((a, b) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      });
+
+      // Then pull out what we care about
       system.commands.forEach((command) => {
         commands = commands.concat({
           id: command.id,
-          namespace: system.namespace,
           name: command.name,
-          system: system.display_name || system.name,
+          system: system.name,
+          hidden: command.hidden,
           version: system.version,
           description: command.description || 'No Description Provided',
         });
       });
     });
 
+    // $scope.response = response['systems'];
     $scope.response = response;
     $scope.data = commands;
   };
@@ -48,5 +98,31 @@ export default function commandIndexController(
     $scope.data = {};
   };
 
-  $scope.successCallback($rootScope.sysResponse);
+  $scope.successCallback(detailSystems);
+
+  // const loadCommands = function() {
+  //   $scope.response = undefined;
+  //   $scope.data = {};
+
+  //   $rootScope.systemsPromise.then(
+  //     () => {
+  //       // We don't actually use the CommandService.getCommands(), but make the
+  //       // call just to verify that the user has bg-command-read
+  //       $q.all({
+  //         commands: CommandService.getCommands(),
+  //         systems: SystemService.getSystems(),
+  //       }).then(
+  //         $scope.successCallback,
+  //         $scope.failureCallback
+  //       );
+  //     },
+  //     $scope.failureCallback
+  //   );
+  // };
+
+  // $scope.$on('userChange', function() {
+  //   loadCommands();
+  // });
+
+  // loadCommands();
 };
