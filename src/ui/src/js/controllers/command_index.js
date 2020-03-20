acb4f7542ab56fbe8174263723da6b788e8318c2
Matt Patrick <hazmat345@gmail.com>
2020-03-19 20:55:00 -0400

    Cleaning up command index page

--- src/ui/src/js/controllers/command_index.js
+++ src/ui/src/js/controllers/command_index.js
@@ -2,85 +2,43 @@
 commandIndexController.$inject = [
   '$rootScope',
   '$scope',
-  '$q',
-  'SystemService',
-  'CommandService',
   'DTOptionsBuilder',
-  'detailSystems',
-  'commands',
 ];
 
 /**
  * commandIndexController - Angular controller for all commands page.
  * @param  {$rootScope} $rootScope   Angular's $rootScope object.
  * @param  {$scope} $scope           Angular's $scope object.
- * @param  {$q} $q                   Angular's $q object.
- * @param  {Object} SystemService    Beer-Garden system service.
- * @param  {Object} CommandService   Beer-Garden command service.
  * @param  {Object} DTOptionsBuilder Data-tables' builder for options.
  */
 export default function commandIndexController(
-  $rootScope,
-  $scope,
-  $q,
-  SystemService,
-  CommandService,
-  DTOptionsBuilder,
-  detailSystems,
-  commands) {
+    $rootScope,
+    $scope,
+    DTOptionsBuilder) {
   $scope.setWindowTitle('commands');
 
   $scope.dtOptions = DTOptionsBuilder.newOptions()
-    .withOption('order', [4, 'asc'])
+    .withOption('order', [[0, 'asc'], [1, 'asc'], [2, 'asc'], [3, 'asc']])
     .withOption('autoWidth', false)
     .withBootstrap();
 
-  $scope.stateParams = function(entry) {
-    return {
-      id: entry.id,
-      name: entry.name,
-      systemName: entry.system,
-      systemVersion: $rootScope.getVersionForUrl(entry.version),
-    };
-  };
-
   $scope.successCallback = function(response) {
-  // $scope.successCallback = function(systems) {
-    // let systems = response['systems'].data;
-    let systems = response.data;
+    // Pull out what we care about
     let commands = [];
 
-    // Sort the systems
-    systems.sort((a, b) => {
-      let aName = a.display_name || a.name;
-      let bName = b.display_name || b.name;
-
-      if (aName < bName) return -1;
-      if (aName > bName) return 1;
-      return 0;
-    });
-
-    systems.forEach((system) => {
-      // Sort the commands
-      system.commands.sort((a, b) => {
-        if (a.name < b.name) return -1;
-        if (a.name > b.name) return 1;
-        return 0;
-      });
-
-      // Then pull out what we care about
+    response.data.forEach((system) => {
       system.commands.forEach((command) => {
         commands = commands.concat({
           id: command.id,
+          namespace: system.namespace,
           name: command.name,
-          system: system.name,
+          system: system.display_name || system.name,
           version: system.version,
           description: command.description || 'No Description Provided',
         });
       });
     });
 
-    // $scope.response = response['systems'];
     $scope.response = response;
     $scope.data = commands;
   };
@@ -90,31 +48,5 @@ export default function commandIndexController(
     $scope.data = {};
   };
 
-  $scope.successCallback(detailSystems);
-
-  // const loadCommands = function() {
-  //   $scope.response = undefined;
-  //   $scope.data = {};
-
-  //   $rootScope.systemsPromise.then(
-  //     () => {
-  //       // We don't actually use the CommandService.getCommands(), but make the
-  //       // call just to verify that the user has bg-command-read
-  //       $q.all({
-  //         commands: CommandService.getCommands(),
-  //         systems: SystemService.getSystems(),
-  //       }).then(
-  //         $scope.successCallback,
-  //         $scope.failureCallback
-  //       );
-  //     },
-  //     $scope.failureCallback
-  //   );
-  // };
-
-  // $scope.$on('userChange', function() {
-  //   loadCommands();
-  // });
-
-  // loadCommands();
+  $scope.successCallback($rootScope.sysResponse);
 };
