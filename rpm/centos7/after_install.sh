54542dca49f93f5bed0274912f8cf804fa68652b
Matt Patrick <hazmat345@gmail.com>
2020-03-24 17:02:25 -0400

    Consolidating services and configs in rpm install scripts

--- rpm/centos7/after_install.sh
+++ rpm/centos7/after_install.sh
@@ -9,13 +9,9 @@ BIN_HOME="$APP_HOME/bin"
 PLUGIN_LOG_HOME="$LOG_HOME/plugins"
 PLUGIN_HOME="$APP_HOME/plugins"
 
-BARTENDER_CONFIG="${CONFIG_HOME}/bartender-config"
-BARTENDER_LOG_CONFIG="${CONFIG_HOME}/bartender-logging-config.json"
-BARTENDER_LOG_FILE="$LOG_HOME/bartender.log"
-
-BREW_VIEW_CONFIG="${CONFIG_HOME}/brew-view-config"
-BREW_VIEW_LOG_CONFIG="${CONFIG_HOME}/brew-view-logging-config.json"
-BREW_VIEW_LOG_FILE="$LOG_HOME/brew-view.log"
+CONFIG_FILE="${CONFIG_HOME}/config.yaml"
+LOG_CONFIG="${CONFIG_HOME}/logging.yaml"
+LOG_FILE="$LOG_HOME/beer-garden.log"
 
 case "$1" in
     1)
@@ -37,52 +33,25 @@ case "$1" in
             mkdir -p "$PLUGIN_HOME"
         fi
 
-        # Generate logging configs if they don't exist
-        if [ ! -f "$BARTENDER_LOG_CONFIG" ]; then
-            "$APP_HOME/bin/generate_bartender_log_config" \
-                --log-config-file "$BARTENDER_LOG_CONFIG" \
-                --log-file "$BARTENDER_LOG_FILE" \
+        # Generate logging config if it doesn't exist
+        if [ ! -f "$LOG_CONFIG" ]; then
+            "$APP_HOME/bin/generate_log_config" \
+                --log-config-file "$LOG_CONFIG" \
+                --log-file "$LOG_FILE" \
                 --log-level "WARN"
         fi
 
-        if [ ! -f "$BREW_VIEW_LOG_CONFIG" ]; then
-            "$APP_HOME/bin/generate_brew_view_log_config" \
-                --log-config-file "$BREW_VIEW_LOG_CONFIG" \
-                --log-file "$BREW_VIEW_LOG_FILE" \
-                --log-level "WARN"
-        fi
-
-        # Enforce .yaml extension for yaml config files
-        if [ -f "$BARTENDER_CONFIG.yml" ]; then
-            mv "$BARTENDER_CONFIG.yml" "$BARTENDER_CONFIG.yaml"
-        fi
-        if [ -f "$BREW_VIEW_CONFIG.yml" ]; then
-            mv "$BREW_VIEW_CONFIG.yml" "$BREW_VIEW_CONFIG.yaml"
-        fi
-
-        # Generate application configs if they don't exist
-        # Migrate them if they do, converting to yaml if necessary
-        if [ -f "$BARTENDER_CONFIG.yaml" ]; then
-            "$APP_HOME/bin/migrate_bartender_config" -c "$BARTENDER_CONFIG.yaml"
-        elif [ -f "$BARTENDER_CONFIG.json" ]; then
-            "$APP_HOME/bin/migrate_bartender_config" -c "$BARTENDER_CONFIG.json" -t "yaml"
+        # Generate or migrate application config
+        if [ -f "$CONFIG_FILE" ]; then
+            "$APP_HOME/bin/migrate_config" -c "$CONFIG_FILE"
         else
-            "$APP_HOME/bin/generate_bartender_config" \
-                -c "$BARTENDER_CONFIG.yaml" -l "$BARTENDER_LOG_CONFIG" \
+            "$APP_HOME/bin/generate_config" \
+                -c "$CONFIG_FILE" -l "$LOG_CONFIG" \
                 --plugin-local-directory "$PLUGIN_HOME" \
                 --plugin-local-log-directory "$PLUGIN_LOG_HOME"
         fi
 
-        if [ -f "$BREW_VIEW_CONFIG.yaml" ]; then
-            "$APP_HOME/bin/migrate_brew_view_config" -c "$BREW_VIEW_CONFIG.yaml"
-        elif [ -f "$BREW_VIEW_CONFIG.json" ]; then
-            "$APP_HOME/bin/migrate_brew_view_config" -c "$BREW_VIEW_CONFIG.json" -t "yaml"
-        else
-            "$APP_HOME/bin/generate_brew_view_config" \
-                -c "$BREW_VIEW_CONFIG.yaml" -l "$BREW_VIEW_LOG_CONFIG"
-        fi
-
-        # Reload units so service is ready to go
+        # Reload units
         systemctl daemon-reload
     ;;
     2)
