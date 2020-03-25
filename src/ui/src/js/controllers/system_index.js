8416cc72dbcfe201649f032c124f9cc1e5bbe498
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2020-03-25 13:29:16 -0400

    Got search bar working

--- src/ui/src/js/controllers/system_index.js
+++ src/ui/src/js/controllers/system_index.js
@@ -6,6 +6,7 @@ systemIndexController.$inject = [
   '$stateParams',
   'SystemService',
   'UtilityService',
+  'DTOptionsBuilder',
 ];
 
 /**
@@ -21,11 +22,26 @@ export default function systemIndexController(
     $rootScope,
     $state,
     $stateParams,
+    SystemService,
     UtilityService,
+    DTOptionsBuilder
     ) {
   $scope.setWindowTitle();
 
   $scope.util = UtilityService;
+
+  $scope.dtOptions = DTOptionsBuilder.newOptions()
+    .withOption('autoWidth', false)
+    .withOption('order', [[0, 'asc'], [1, 'asc'], [2, 'asc'], [3, 'asc']])
+    .withLightColumnFilter({
+      0: {html: 'input', type: 'text', attr: {class: 'form-inline form-control'}},
+      1: {html: 'input', type: 'text', attr: {class: 'form-inline form-control'}},
+      2: {html: 'input', type: 'text', attr: {class: 'form-inline form-control'}},
+      3: {html: 'input', type: 'text', attr: {class: 'form-inline form-control'}},
+      4: {html: 'input', type: 'number', attr: {class: 'form-inline form-control'}},
+      5: {html: 'input', type: 'number', attr: {class: 'form-inline form-control'}},
+    })
+    .withBootstrap();
 
   $scope.successCallback = function(response) {
     $scope.response = response;
