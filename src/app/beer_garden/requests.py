66efd40b13eace0a6c92b2bfbcf71c4e6cfd8efb
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2020-06-19 12:07:09 -0400

    Resolving Comment

--- src/app/beer_garden/requests.py
+++ src/app/beer_garden/requests.py
@@ -744,7 +744,7 @@ def handle_event(event):
 
             db.update(existing_request)
 
-    # Required if the main thread spawns a wait Request
+    # Required if the main process spawns a wait Request
     if event.name == Events.REQUEST_COMPLETED.name:
         if str(event.payload.id) in request_map:
             request_map[str(event.payload.id)].set()
