2abeee7a29147d66a7c0c9988d7a73f58c216cf8
Matt Patrick <hazmat345@gmail.com>
2020-07-08 16:38:53 -0400

    Reverting json node counting check

--- src/ui/src/js/controllers/request_view.js
+++ src/ui/src/js/controllers/request_view.js
@@ -114,7 +114,7 @@ export default function requestViewController(
         try {
           let parsedOutput = JSON.parse(rawOutput);
           rawOutput = $scope.stringify(parsedOutput);
-          if ($scope.memorySizeOf($scope.formattedOutput) < 5) {
+          if ($scope.countNodes($scope.formattedOutput) < 1000) {
             $scope.jsonOutput = rawOutput;
             $scope.formattedAvailable = true;
             $scope.showFormatted = true;
@@ -274,36 +274,17 @@ export default function requestViewController(
     return JSON.stringify(data, undefined, 2);
   };
 
-  $scope.memorySizeOf = function(obj) {
-    let bytes = 0;
+  $scope.countNodes = function(obj) {
+    // Arrays have type object too
+    if (typeof obj != 'object') {
+      return 1;
+    }
 
-    function sizeOf(obj) {
-        if(obj !== null && obj !== undefined) {
-            switch(typeof obj) {
-            case 'number':
-                bytes += 8;
-                break;
-            case 'string':
-                bytes += obj.length * 2;
-                break;
-            case 'boolean':
-                bytes += 4;
-                break;
-            case 'object':
-                var objClass = Object.prototype.toString.call(obj).slice(8, -1);
-                if(objClass === 'Object' || objClass === 'Array') {
-                    for(var key in obj) {
-                        if(!obj.hasOwnProperty(key)) continue;
-                        sizeOf(obj[key]);
-                    }
-                } else bytes += obj.toString().length * 2;
-                break;
-            }
-        }
-        return bytes;
-    };
-
-    return(sizeOf(obj) / 1048576).toFixed(3)
+    let total = 1;
+    for (const key of Object.keys(obj)) {
+      total += $scope.countNodes(object[key]);
+    }
+    return total;
   };
 
   function eventCallback(event) {
