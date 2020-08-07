73f49be849bc3ee11d5942a0a71b3e1f843d27b9
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2020-08-07 10:47:56 -0400

    Changed URL Config

--- src/ui/src/js/services/instance_service.js
+++ src/ui/src/js/services/instance_service.js
@@ -18,7 +18,7 @@ export default function instanceService($http) {
         return $http.get('api/v1/instances/' + instanceId)
     },
     getInstanceLogs: (instanceId, wait_timeout, start_line, end_line) => {
-      return $http.get('api/v1/instances/logs/' + instanceId, {
+      return $http.get('api/v1/instances/' + instanceId + '/logs/', {
         params: {
           "start_line":start_line,
           "end_line":end_line,
