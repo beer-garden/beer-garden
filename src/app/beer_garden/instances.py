b807eda807d74b6fa55e14363e72495610bdf627
Matt Patrick <hazmat345@gmail.com>
2020-09-11 15:33:34 -0400

    Fixing instances module functions

--- src/app/beer_garden/instances.py
+++ src/app/beer_garden/instances.py
@@ -1,41 +1,76 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from brewtils.models import Events, Instance
+from brewtils.errors import BrewtilsException
+from brewtils.models import Events, Instance, System
 
-import beer_garden.db.api as db
 import beer_garden.config as config
+import beer_garden.db.api as db
+from beer_garden.errors import NotFoundException
 
 logger = logging.getLogger(__name__)
 
 
-def get_instance(instance_id: str = None, instance: Instance = None, **_) -> Instance:
+def get_instance(
+    instance_id: str = None,
+    system_id: str = None,
+    instance_name: str = None,
+    instance: Instance = None,
+    **_,
+) -> Instance:
     """Retrieve an individual Instance
 
     Args:
         instance_id: The Instance ID
+        system_id: The System ID
+        instance_name: The Instance name
         instance: The Instance
 
     Returns:
         The Instance
 
     """
-    return instance or db.query_unique(Instance, id=instance_id)
+    if instance:
+        return instance
+
+    if system_id and instance_name:
+        system = db.query_unique(System, raise_missing=True, id=system_id)
+
+        try:
+            return system.get_instance_by_name(instance_name, raise_missing=True)
+        except BrewtilsException:
+            raise NotFoundException(
+                f"System {system} does not have an instance with name '{instance_name}'"
+            ) from None
+
+    elif instance_id:
+        system = db.query_unique(System, raise_missing=True, instances__id=instance_id)
+
+        try:
+            return system.get_instance_by_id(instance_id, raise_missing=True)
+        except BrewtilsException:
+            raise NotFoundException(
+                f"System {system} does not have an instance with id '{instance_id}'"
+            ) from None
+
+    raise NotFoundException()
 
 
-def remove_instance(instance_id: str = None, instance: Instance = None, **_) -> None:
+def remove_instance(
+    *_, system: System = None, instance: Instance = None, **__
+) -> Instance:
     """Removes an Instance
 
     Args:
-        instance_id: The Instance ID
+        system: The System
         instance: The Instance
 
     Returns:
-        None
+        The deleted Instance
     """
-    instance = instance or db.query_unique(Instance, id=instance_id)
+    db.modify(system, pull__instances=instance)
 
-    db.delete(instance)
+    return instance
 
 
 def handle_event(event):
