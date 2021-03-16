c201d86b128cb22a110bfe4a11c7f764fb0baab7
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2021-03-16 12:42:28 -0400

    Multiple fixtures

--- test/integration/gardens/setup/garden_setup_test.py
+++ test/integration/gardens/setup/garden_setup_test.py
@@ -8,6 +8,6 @@ class TestGardenSetup(object):
     def test_system_register_successful(self):
 
         parser = SchemaParser()
-        gardens = parser.parse_garden(self.easy_client.client.session.get("api/v1/gardens/"))
+        gardens = parser.parse_garden(self.easy_client.client.session.get(self.easy_client.client.base_url + "api/v1/gardens/"))
 
         assert len(gardens) == 1
