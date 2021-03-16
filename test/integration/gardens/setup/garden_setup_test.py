4492d79b6e10d37c9f4043b47137314a77817c1c
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2021-03-16 13:17:32 -0400

    Add check for Parent System push

--- test/integration/gardens/setup/garden_setup_test.py
+++ test/integration/gardens/setup/garden_setup_test.py
@@ -5,7 +5,7 @@ from helper.assertion import assert_system_running
 @pytest.mark.usefixtures('easy_client', 'parser', 'child_easy_client')
 class TestGardenSetup(object):
 
-    def test_garden_register_successful(self):
+    def test_garden_auto_register_successful(self):
 
         #parser = SchemaParser()
         response = self.easy_client.client.session.get(self.easy_client.client.base_url + "api/v1/gardens/")
@@ -13,7 +13,7 @@ class TestGardenSetup(object):
         gardens = self.parser.parse_garden(response.json(), many=True)
 
         print(gardens)
-        assert len(gardens) == 1
+        assert len(gardens) == 2
 
     def test_child_systems_register_successful(self):
 
@@ -28,5 +28,20 @@ class TestGardenSetup(object):
                 namespaces[system.namespace] += 1
 
         print(namespaces)
-        assert namespaces["child"] > 1
+        assert namespaces["child-docker"] > 0
+
+    def test_child_systems_register_successful(self):
+
+        systems = self.easy_client.find_systems()
+
+        namespaces = dict()
+
+        for system in systems:
+            if system.namespace not in namespaces.keys():
+                namespaces[system.namespace] = 1
+            else:
+                namespaces[system.namespace] += 1
+
+        print(namespaces)
+        assert namespaces["child-docker"] > 0
 
