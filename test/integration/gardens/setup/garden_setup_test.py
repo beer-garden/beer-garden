5353fc88d3733ffba94d1b37aac83bcd3b7bbae1
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2021-03-16 14:08:19 -0400

    Register child garden

--- test/integration/gardens/setup/garden_setup_test.py
+++ test/integration/gardens/setup/garden_setup_test.py
@@ -1,6 +1,8 @@
 import pytest
 from brewtils.schema_parser import SchemaParser
+from brewtils.models import Garden
 from helper.assertion import assert_system_running
+
 
 @pytest.mark.usefixtures('easy_client', 'parser', 'child_easy_client')
 class TestGardenSetup(object):
@@ -13,6 +15,28 @@ class TestGardenSetup(object):
 
         print(gardens)
         assert len(gardens) == 2
+
+    def test_garden_register(self):
+
+        child_garden = Garden(name="child-docker", connection_type="HTTP",
+                              connection_params={"host": "beer-garden-child", "port": 2347, "ssl": False})
+
+        payload = self.parser.serialize_garden(child_garden)
+        response = self.easy_client.client.session.post(
+            self.easy_client.client.base_url + "api/v1/gardens/", data=payload, headers=self.easy_client.client.JSON_HEADERS
+        )
+
+        assert response.ok
+
+        response = self.easy_client.client.session.get(self.easy_client.client.base_url + "api/v1/gardens/")
+
+        gardens = self.parser.parse_garden(response.json(), many=True)
+
+        print(gardens)
+        assert len(gardens) == 3
+
+
+
 
     def test_child_systems_register_successful(self):
 
@@ -43,4 +67,3 @@ class TestGardenSetup(object):
 
         print(namespaces)
         assert "child-docker" in namespaces.keys() and namespaces["child-docker"] > 0
-
