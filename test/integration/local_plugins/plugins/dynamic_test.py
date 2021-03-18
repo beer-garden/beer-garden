d82e2f9b8ec4088c1daa051ef8590d3e055138f7
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2021-03-18 08:51:53 -0400

    Seperate Remote and Local Tests

---
+++ test/integration/local_plugins/plugins/dynamic_test.py
@@ -0,0 +1,75 @@
+import pytest
+
+try:
+    from helper import wait_for_response
+    from helper.assertion import assert_successful_request, assert_validation_error
+except:
+    from ...helper import wait_for_response
+    from ...helper.assertion import assert_successful_request, assert_validation_error
+
+@pytest.fixture(scope="class")
+def system_spec():
+    return {'system': 'dynamic', 'system_version': '3.0.0.dev0', 'instance_name': 'd1'}
+
+
+@pytest.mark.usefixtures('easy_client', 'request_generator')
+class TestDynamic(object):
+
+    def test_say_specific_in_choice(self):
+        request = self.request_generator.generate_request(command="say_specific",
+                                                          parameters={"message": "a"})
+        response = wait_for_response(self.easy_client, request)
+        assert_successful_request(response, output="a")
+
+    def test_say_specific_not_in_choice(self):
+        request = self.request_generator.generate_request(command="say_specific",
+                                                          parameters={"message": "NOT_IN_CHOICES"})
+        assert_validation_error(self, self.easy_client, request, regex='not a valid choice')
+
+    def test_say_specific_from_command_in_choice(self):
+        request = self.request_generator.generate_request(command="say_specific_from_command",
+                                                          parameters={"message": "a"})
+        response = wait_for_response(self.easy_client, request)
+        assert_successful_request(response, output="a")
+
+    def test_say_specific_from_command_not_in_choice(self):
+        request = self.request_generator.generate_request(command="say_specific_from_command",
+                                                          parameters={"message": "NOT_IN_CHOICES"})
+        assert_validation_error(self, self.easy_client, request, regex='not a valid choice')
+
+    def test_say_specific_from_command_nullable_message(self):
+        request = self.request_generator.generate_request(command="say_specific_from_command_nullable",
+                                                          parameters={"message": None})
+        response = wait_for_response(self.easy_client, request)
+        assert_successful_request(response, output="null")
+
+    def test_say_specific_from_command_nullable_not_in_choice(self):
+        request = self.request_generator.generate_request(command="say_specific_from_command_nullable",
+                                                          parameters={"message": "NOT_IN_CHOICES"})
+        assert_validation_error(self, self.easy_client, request, regex='not a valid choice')
+
+    @pytest.mark.skip("Skipping until we find a URL to demonstrate the capability.")
+    def test_say_specific_from_url_good_choice(self):
+        request = self.request_generator.generate_request(command="say_specific_from_url",
+                                                          parameters={"message": "Kentucky"})
+        response = wait_for_response(self.easy_client, request)
+        assert_successful_request(response, output="Kentucky")
+
+    @pytest.mark.skip("Skipping until we find a URL to demonstrate the capability.")
+    def test_say_specific_from_url_bad_choice(self):
+        request = self.request_generator.generate_request(command="say_specific_from_url",
+                                                          parameters={"message": "NOT_IN_CHOICES"})
+        assert_validation_error(self, self.easy_client, request, regex='not a valid choice')
+
+    @pytest.mark.skip("Skipping until we find a URL to demonstrate the capability.")
+    def test_say_specific_from_url_nullable(self):
+        request = self.request_generator.generate_request(command="say_specific_from_url_nullable",
+                                                          parameters={"message": None})
+        response = wait_for_response(self.easy_client, request)
+        assert_successful_request(response, output="null")
+
+    def test_say_specific_non_strict_out_of_choice(self):
+        request = self.request_generator.generate_request(command="say_specific_non_strict_typeahead",
+                                                          parameters={"message": "NOT_IN_CHOICE"})
+        response = wait_for_response(self.easy_client, request)
+        assert_successful_request(response, output="NOT_IN_CHOICE")
---
+++ test/integration/local_plugins/plugins/echo_sleeper_test.py
@@ -0,0 +1,48 @@
+import pytest
+
+try:
+    from helper import wait_for_response
+    from helper.assertion import assert_successful_request, assert_errored_request
+except:
+    from ...helper import wait_for_response
+    from ...helper.assertion import assert_successful_request, assert_errored_request
+
+
+@pytest.fixture(scope="class")
+def system_spec():
+    return {'system': 'echo-sleeper', 'system_version': '3.0.0.dev0', 'instance_name': 'default'}
+
+
+@pytest.mark.usefixtures('easy_client', 'request_generator')
+class TestEchoSleeper(object):
+
+    def test_parent_with_children_success(self):
+        request = self.request_generator.generate_request(command="say_sleep", parameters={"message": "foo",
+                                                                                           "amount": 0.01})
+        response = wait_for_response(self.easy_client, request)
+        assert_successful_request(response)
+        assert len(response.children) == 2
+        for child_request in response.children:
+            assert_successful_request(child_request)
+
+    def test_parent_with_error_does_not_raise(self):
+        request = self.request_generator.generate_request(command="say_error_and_catch", parameters={"message": "foo"})
+        response = wait_for_response(self.easy_client, request)
+        assert_successful_request(response)
+        assert len(response.children) == 2
+        for child_request in response.children:
+            if child_request.system == "echo":
+                assert_successful_request(child_request)
+            elif child_request.system == "error":
+                assert_errored_request(child_request)
+
+    def test_parent_with_error_and_raise(self):
+        request = self.request_generator.generate_request(command="say_error_and_raise", parameters={"message": "foo"})
+        response = wait_for_response(self.easy_client, request)
+        assert_errored_request(response)
+        assert len(response.children) == 2
+        for child_request in response.children:
+            if child_request.system == "echo":
+                assert_successful_request(child_request)
+            elif child_request.system == "error":
+                assert_errored_request(child_request)
