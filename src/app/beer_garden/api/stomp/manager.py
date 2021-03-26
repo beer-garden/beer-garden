6d54ccc92a8c60899c72ca7d8936928415316cd1
Matt Patrick <hazmat345@gmail.com>
2021-03-26 18:51:41 -0400

    Adding some line breaks

--- src/app/beer_garden/api/stomp/manager.py
+++ src/app/beer_garden/api/stomp/manager.py
@@ -161,9 +161,11 @@ class StompManager(StoppableThread):
         for key in list(self.conn_dict):
             if not key == skip_key:
                 gardens = self.conn_dict[key]["gardens"]
+
                 for garden in gardens:
                     if garden_name == garden["name"] and not garden["main"]:
                         gardens.remove(garden)
+
                 if not gardens:
                     self.conn_dict[key]["conn"].disconnect()
                     self.conn_dict.pop(key)
@@ -174,6 +176,7 @@ class StompManager(StoppableThread):
 
         elif event.name == Events.GARDEN_UPDATED.name:
             skip_key = None
+
             if event.payload.connection_type:
                 if event.payload.connection_type.casefold() == "stomp":
                     stomp_config = self.format_connection_params(
@@ -183,9 +186,11 @@ class StompManager(StoppableThread):
                     skip_key = self.add_connection(
                         stomp_config=stomp_config, name=event.payload.name
                     )
+
             self.remove_garden_from_list(
                 garden_name=event.payload.name, skip_key=skip_key
             )
+
         for value in self.conn_dict.values():
             conn = value["conn"]
             if conn:
@@ -201,14 +206,18 @@ class StompManager(StoppableThread):
         tmp_headers = {}
         key_to_key = None
         key_to_value = None
+
         for header in headers:
             header = eval(header)
+
             for key in header.keys():
                 if "key" in key:
                     key_to_key = key
                 elif "value" in key:
                     key_to_value = key
+
             tmp_headers[header[key_to_key]] = header[key_to_value]
+
         return tmp_headers
 
     def add_connection(self, stomp_config=None, name=None, is_main=False):
@@ -217,6 +226,7 @@ class StompManager(StoppableThread):
         ssl = stomp_config.get("ssl")
         use_ssl = ssl.get("use_ssl") or False
         conn_dict_key = f"{host_and_ports}{subscribe_destination}{use_ssl}"
+
         if conn_dict_key in self.conn_dict:
             if {"name": name, "main": is_main} not in self.conn_dict[conn_dict_key][
                 "gardens"
@@ -229,12 +239,16 @@ class StompManager(StoppableThread):
                 "conn": self.connect(stomp_config, [{"name": name, "main": is_main}]),
                 "gardens": [{"name": name, "main": is_main}],
             }
+
         if "headers_list" not in self.conn_dict:
             self.conn_dict[conn_dict_key]["headers_list"] = []
+
         if stomp_config.get("headers") and is_main:
             headers = self.convert_header_to_dict(stomp_config.get("headers"))
+
             if headers not in self.conn_dict[conn_dict_key]["headers_list"]:
                 self.conn_dict[conn_dict_key]["headers_list"].append(headers)
+
         return conn_dict_key
 
     @staticmethod
