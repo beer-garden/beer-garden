a5df8aedf7d8d618d038f85d1229a6023ec91a03
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2021-03-30 12:39:20 -0400

    Updated logging

--- test/integration/stomp/publisher/create_request_test.py
+++ test/integration/stomp/publisher/create_request_test.py
@@ -39,12 +39,12 @@ class MessageListener(object):
         try:
             if headers['model_class'] == 'Operation':
 
-                #parsed = json.loads(message)
                 parsed = SchemaParser.parse_operation(message, from_string=True)
 
-                print(message)
-                if parsed.model and parsed.model.name and parsed.model.name.startswith("REQUEST"):
-                    self.create_event_captured = True
+                if parsed.model and parsed.model.name:
+                    print(parsed.model.name)
+                    if parsed.model.name.startswith("REQUEST"):
+                        self.create_event_captured = True
             elif headers['model_class'] == 'error_message':
                 print("Error Message Returned:", message)
         except:
@@ -99,20 +99,18 @@ class TestPublisher(object):
             destination="Beer_Garden_Operations",
         )
 
-        time.sleep(30)
+        time.sleep(10)
 
         requests = self.easy_client.find_requests()
 
         found_request = False
 
-        # print(len(requests))
-
         for request in requests:
-            # print(request.metadata)
             if "generated-by" in request.metadata and request.metadata["generated-by"] == "test_publish_create_request":
                 found_request = True
                 break
 
+        assert listener.create_event_captured
         assert found_request
 
     @pytest.mark.usefixtures('easy_client', 'request_generator')
@@ -120,8 +118,6 @@ class TestPublisher(object):
         """Published the Request over HTTP and verifies of STOMP"""
 
         request_model = self.request_generator.generate_request(parameters={"message": "test_string", "loud": True})
-
-        request_model['metadata'] = {"generated-by": "test_listen_create_request"}
 
         listener = MessageListener()
         stomp_connection.set_listener('', listener)
