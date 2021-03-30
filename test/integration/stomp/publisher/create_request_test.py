be4e2ee23e89dbaa3a8a7b8260431b32b3dcd1c4
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2021-03-30 08:27:35 -0400

    Print messages after published

--- test/integration/stomp/publisher/create_request_test.py
+++ test/integration/stomp/publisher/create_request_test.py
@@ -23,21 +23,17 @@ except:
 
 
 class MessageListener(object):
+
+    create_event_captured = False
+
     def on_error(self, headers, message):
         print('received an error %s' % headers)
 
     def on_message(self, headers, message):
-        global conn
         try:
             parsed = SchemaParser.parse(message, from_string=True, model_class=eval(headers['model_class']))
             print("Parsed message:", parsed)
 
-            #  Forwards an event object to a destination if payload has a metadata
-            try:
-                if 'reply-to' in parsed.payload.metadata:
-                    conn.send(body=message, headers=headers, destination=parsed.payload.metadata['reply-to'])
-            except AttributeError:
-                pass
         except AttributeError:
             print("Error: unable to parse message:", message)
 
