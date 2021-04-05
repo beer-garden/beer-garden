4e69acaaba52a6e997b1d09018b53fc5d2814cf1
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2021-04-05 14:13:01 -0400

    Testing bad fix with parent / child comms

--- src/app/beer_garden/router.py
+++ src/app/beer_garden/router.py
@@ -546,6 +546,9 @@ def _determine_target_garden(operation: Operation) -> str:
             return _instance_id_lookup(operation.args[0])
 
     elif operation.operation_type == "REQUEST_CREATE":
+
+        # TODO DONT LET THIS INTO THE BASELINE!!!!
+        setup_routing()
         target_system = System(
             namespace=operation.model.namespace,
             name=operation.model.system,
