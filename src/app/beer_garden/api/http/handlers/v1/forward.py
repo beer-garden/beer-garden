57a131b703b84d7c1e893c3d0107d896a698b2f2
Matt Patrick <hazmat345@gmail.com>
2021-06-08 21:28:07 -0400

    Forward API now accepts blocking query params

--- src/app/beer_garden/api/http/handlers/v1/forward.py
+++ src/app/beer_garden/api/http/handlers/v1/forward.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import asyncio
+
+from brewtils.errors import TimeoutExceededError
 from brewtils.schema_parser import SchemaParser
 
 from beer_garden.api.http.authorization import Permissions, authenticated
@@ -17,6 +20,18 @@ class ForwardAPI(BaseHandler):
             When a Beer Garden needs to forward a request, this API will support routing
             to all CRUD actions exposed by the entry points.
         parameters:
+          - name: blocking
+            in: query
+            required: false
+            description: Flag indicating whether to wait for request completion
+            type: boolean
+            default: false
+          - name: timeout
+            in: query
+            required: false
+            description: Max seconds to wait for request completion. (-1 = wait forever)
+            type: float
+            default: -1
           - name: forward
             in: body
             required: true
@@ -37,7 +52,19 @@ class ForwardAPI(BaseHandler):
             self.request.decoded_body, from_string=True
         )
 
-        response = await self.client(operation)
+        task = asyncio.create_task(self.client(operation))
 
-        self.set_header("Content-Type", "application/json; charset=UTF-8")
-        self.write(response)
+        # Deal with blocking
+        blocking = self.get_argument("blocking", default="").lower() == "true"
+        if not blocking:
+            self.set_status(204)
+        else:
+            timeout = float(self.get_argument("timeout", default="-1"))
+
+            done, _ = await asyncio.wait({task}, timeout=timeout)
+
+            if not done:
+                raise TimeoutExceededError("Timeout exceeded")
+
+            self.set_header("Content-Type", "application/json; charset=UTF-8")
+            self.write(done.result())
