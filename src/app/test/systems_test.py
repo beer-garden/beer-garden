774e701cac64ccd2fa8e3bb80cc2b6b04ded12b4
Scott Taubman <86254668+scott-taubman@users.noreply.github.com>
2021-07-26 11:39:36 -0400

    #958 - Unit test refactor

--- src/app/test/systems_test.py
+++ src/app/test/systems_test.py
@@ -1,47 +1,47 @@
 # -*- coding: utf-8 -*-
 import pytest
-from mock import Mock
-from mongoengine import connect
-from brewtils.models import System, Command
 from brewtils.errors import ModelValidationError
+from brewtils.models import Command, System
+from mongoengine import connect
 
-import beer_garden
-import beer_garden.router
+from beer_garden import config
 from beer_garden.systems import create_system, update_system
 
 
-@pytest.fixture()
-def mongo_conn():
-    connect("beer_garden", host="mongomock://localhost")
-
-
-def test_disallow_system_command_updates(mongo_conn, monkeypatch):
-    system = System(
-        name="original",
-        version="v0.0.1",
-        namespace="beer_garden",
-        commands=[Command(name="original")],
-    )
-    create_system(system)
-    monkeypatch.setattr(beer_garden.db.api, "modify", Mock())
-    monkeypatch.setattr(beer_garden.router, "add_routing_system", Mock())
-    try:
-        update_system(system=system, new_commands=[Command(name="changed_command")])
-        raise AssertionError()
-    except ModelValidationError:
-        pass
-
-
-def test_allow_system_command_updates(mongo_conn, monkeypatch):
-    system = System(
-        name="test",
-        version="v0.0.1",
-        namespace="beer_garden",
-        commands=[Command(name="original")],
+@pytest.fixture
+def system():
+    yield create_system(
+        System(
+            name="original",
+            version="v0.0.1",
+            namespace="beer_garden",
+            commands=[Command(name="original")],
+        )
     )
 
-    create_system(system)
-    monkeypatch.setattr(beer_garden.config, "get", lambda x: True)
-    monkeypatch.setattr(beer_garden.db.api, "modify", Mock())
-    monkeypatch.setattr(beer_garden.router, "add_routing_system", Mock())
-    update_system(system=system, new_commands=[Command(name="changed_command")])
+
+class TestSystem:
+    @classmethod
+    def setup_class(cls):
+        connect("beer_garden", host="mongomock://localhost")
+
+    def test_disallow_system_command_updates(self, system):
+        """System commands should not be allowed to update if the
+        allow_system_command_updates config is set to False
+        """
+        config._CONFIG = {"plugin": {"allow_system_command_updates": False}}
+
+        with pytest.raises(ModelValidationError):
+            update_system(system=system, new_commands=[Command(name="changed_command")])
+
+    def test_allow_system_command_updates(self, system):
+        """System commands should be allowed to update if the
+        allow_system_command_updates config is set to True
+        """
+        config._CONFIG = {"plugin": {"allow_system_command_updates": True}}
+        updated_system = update_system(
+            system=system, new_commands=[Command(name="changed_command")]
+        )
+        assert (
+            updated_system.commands[0].name == "changed_command"
+        ), "System command should be updated with the new command name"
