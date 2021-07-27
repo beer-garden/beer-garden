a95275cd8a6f59188d923a87eba2e35fafc90e30
Matt Patrick <hazmat345@gmail.com>
2021-07-27 13:00:02 -0400

    Routing tests

--- src/app/test/router_test.py
+++ src/app/test/router_test.py
@@ -1,105 +1,46 @@
 # -*- coding: utf-8 -*-
 import pytest
-from box import Box
-from brewtils.models import Garden, System
-from brewtils.test.comparable import assert_garden_equal
+from brewtils.models import Operation
 from mock import Mock
 
-import beer_garden.config as config
 import beer_garden.garden
 import beer_garden.router
-
-
-@pytest.fixture(autouse=True)
-def setup():
-    beer_garden.router.gardens = {}
-
-    conf = Box(default_box=True)
-    conf.garden.name = "parent"
-    config.assign(conf, force=True)
+from beer_garden.errors import UnknownGardenException
+from beer_garden.router import _determine_target
 
 
 @pytest.fixture
-def p_sys_1():
-    return System(namespace="p", name="sys", version="1")
+def op():
+    return Operation(source_garden_name="parent")
 
 
-@pytest.fixture
-def p_sys_2():
-    return System(namespace="p", name="sys", version="2")
+class TestDetermineTarget:
+    def test_neither(self, monkeypatch, op):
+        monkeypatch.setattr(
+            beer_garden.router, "_target_from_type", Mock(return_value=None)
+        )
+        with pytest.raises(UnknownGardenException):
+            _determine_target(op)
 
+    def test_target_from_op(self, monkeypatch, op):
+        monkeypatch.setattr(
+            beer_garden.router, "_target_from_type", Mock(return_value=None)
+        )
+        op.target_garden_name = "parent"
 
-@pytest.fixture
-def c_sys_1():
-    return System(namespace="c", name="sys", version="1")
+        assert _determine_target(op) == "parent"
 
+    def test_target_from_type(self, monkeypatch, op):
+        monkeypatch.setattr(
+            beer_garden.router, "_target_from_type", Mock(return_value="parent")
+        )
 
-@pytest.fixture
-def c_sys_2():
-    return System(namespace="c", name="sys", version="2")
+        assert _determine_target(op) == "parent"
 
+    def test_mismatch(self, monkeypatch, op):
+        monkeypatch.setattr(
+            beer_garden.router, "_target_from_type", Mock(return_value="child")
+        )
+        op.target_garden_name = "parent"
 
-@pytest.fixture
-def l_garden():
-    return Garden(
-        name="parent",
-        connection_type="local",
-        systems=[]
-        # name="parent", connection_type="local", systems=[str(p_sys_1), str(p_sys_2)]
-    )
-
-
-@pytest.fixture
-def p_garden(p_sys_1, p_sys_2):
-    return Garden(name="parent", connection_type="local", systems=[p_sys_1, p_sys_2])
-
-
-@pytest.fixture
-def c_garden(c_sys_1, c_sys_2):
-    return Garden(name="child", connection_type="http", systems=[c_sys_1, c_sys_2])
-
-
-@pytest.fixture
-def get_gardens_mock(monkeypatch):
-    mock = Mock(return_value=[])
-    monkeypatch.setattr(beer_garden.router, "get_gardens", mock)
-    return mock
-
-
-@pytest.fixture
-def get_local_garden_mock(monkeypatch):
-    mock = Mock(return_value=[])
-    monkeypatch.setattr(beer_garden.router, "local_garden", mock)
-    return mock
-
-
-@pytest.mark.skip
-class TestSetupRouting:
-    def test_all(self, get_gardens_mock, get_local_garden_mock, p_garden, c_garden):
-        get_gardens_mock.return_value = [c_garden]
-        get_local_garden_mock.return_value = p_garden
-
-        beer_garden.router.setup_routing()
-
-        assert str(c_garden) in beer_garden.router.gardens
-        assert str(p_garden) in beer_garden.router.gardens
-
-        assert_garden_equal(c_garden, beer_garden.router.gardens[str(c_garden)])
-        assert_garden_equal(p_garden, beer_garden.router.gardens[str(p_garden)])
-
-    def test_ignore_local(
-        self, get_gardens_mock, get_local_garden_mock, l_garden, c_garden
-    ):
-        """Systems in p_garden should be ignored"""
-        get_gardens_mock.return_value = [c_garden]
-        get_local_garden_mock.return_value = l_garden
-
-        beer_garden.router.setup_routing()
-
-        assert str(c_garden) in beer_garden.router.gardens
-        assert str(l_garden) in beer_garden.router.gardens
-
-        assert_garden_equal(c_garden, beer_garden.router.gardens[str(c_garden)])
-
-        # "l_garden" is the local garden so it shouldn't have any systems
-        assert not beer_garden.router.gardens[str(l_garden)].systems
+        assert _determine_target(op) == "child"
