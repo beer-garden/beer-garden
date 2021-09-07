f8faa5bed611243a2944b55d83b45124ccae61f9
John B <jlrpnbbngtn@gmail.com>
2021-09-07 14:57:27 -0400

    #1067 - Refactored and expanded integration tests

--- test/integration/scheduler/jobs/job_import_export_test.py
+++ test/integration/scheduler/jobs/job_import_export_test.py
@@ -1,20 +1,13 @@
-"""TODO."""
+"""Integration tests for Job import and export."""
 import copy
 from operator import attrgetter
-from typing import Any, Callable, Tuple, Optional
+from time import sleep
+from typing import Any, Callable, List, Optional, Tuple
 
 import pytest
 from brewtils import EasyClient  # type: ignore
-from brewtils.models import (  # type: ignore
-    IntervalTrigger,
-    Job,
-    JobDefinitionList,
-    JobIDList,
-    RequestTemplate,
-)
+from brewtils.models import IntervalTrigger, Job, RequestTemplate  # type: ignore
 
-# TODO: understand when the try block will succeed and when the except
-# TODO: block will execute; i.e. find out why this pattern is necessary
 try:
     from helper import wait_for_response
     from helper.assertion import assert_successful_request, assert_validation_error
@@ -26,7 +19,7 @@ except ImportError:
     )
 
 
-job_name_base = f"import_export_interval_test_{{}}"
+job_name_base = f"import_export_interval_test_{{}}"  # noqa
 job_wait_length = "seconds"
 job_wait_default = 300
 reschedule_key = "reschedule_on_finish"
@@ -69,149 +62,148 @@ def system_spec(request):
     return IntervalTrigger(**trigger_args), RequestTemplate(**req_template_args)
 
 
-# convenience function because we'll be doing a lot of mapping over lists
-def _is_not_none(x: Optional[Any]):
-    return x is not None
+# convenience functions because we'll be doing a lot of mapping over lists
+name_getter: Callable[[Job], str] = attrgetter("name")
+id_getter: Callable[[Job], str] = attrgetter("id")
 
 
 @pytest.mark.usefixtures("easy_client")
 class TestJobImportExport:
-    """Test functionality of job export and job import.
-
-    NOTE: These tests perform a lot of checking of types, lengths of lists, etc. that
-    would be more appropriate in unit tests. Once we have figured out how to run unit
-    tests, many of the asserts can be removed."""
+    """Test functionality of job export and job import."""
 
     easy_client: EasyClient
+
+    def setup_method(self, method):
+        """Make sure there are no Jobs scheduled before testing."""
+        base_wait_time = 0.1  # seconds
+        existing_jobs = self.easy_client.find_jobs()
+
+        for job in existing_jobs:
+            self.easy_client.remove_job(id_getter(job))
+
+        # irritating as this is, the job removal is done by the scheduler, which does
+        # not operate in real time
+        sleep(base_wait_time * len(existing_jobs))
 
     def test_export_interval_jobs(self, system_spec):
         """Test Job export functionality."""
         export_job_count = 5
-        name_getter: Callable[[Job], str] = attrgetter("name")
-        id_getter: Callable[[Job], str] = attrgetter("id")
-
         trigger = system_spec[0]
         req_template = system_spec[1]
 
-        jobs_to_be_created = [
-            self.create_interval_job(idx + 1, trigger, req_template)
-            for idx in range(export_job_count)
-        ]
-        job_creation_responses = list(
-            map(self.easy_client.create_job, jobs_to_be_created)
+        jobs_to_be_created_on_bg = self.make_jobs(
+            export_job_count, trigger, req_template
         )
-
-        # test that none of the responses are 'None' and we have the right count
-        assert all(map(_is_not_none, job_creation_responses))
-        assert len(job_creation_responses) == export_job_count
-
-        jobs_on_beer_garden = [
-            self.easy_client.find_jobs(name=job_name)
-            for job_name in map(name_getter, jobs_to_be_created)
-        ]
-
-        # test that all the returns are not 'None' and are lists of the expected length
-        # and type
-        assert (
-            isinstance(jobs_on_beer_garden, list)
-            and len(jobs_on_beer_garden) == export_job_count
+        jobs_actually_on_beer_garden = self.find_jobs_on_bg_from_job_list(
+            "name", self.create_jobs_on_bg(jobs_to_be_created_on_bg)
         )
-        assert all(map(_is_not_none, jobs_on_beer_garden))
-        assert all(map(lambda x: isinstance(x, list), jobs_on_beer_garden))
-        assert all(map(lambda x: len(x) == 1, jobs_on_beer_garden))
-
-        # we lift them because they are lists, as we've verified
-        jobs_on_beer_garden_lifted = list(map(lambda x: x[0], jobs_on_beer_garden))
-
-        # test that the Jobs returned are actually Jobs
-        assert all(map(_is_not_none, jobs_on_beer_garden_lifted))
-        assert all(map(lambda x: isinstance(x, Job), jobs_on_beer_garden_lifted))
 
         # extract the IDs and use them to export the jobs
-        jobs_on_beer_garden_ids = list(map(id_getter, jobs_on_beer_garden_lifted))
-        job_id_list_for_export = JobIDList(jobs_on_beer_garden_ids)
-        jobs_exported_from_beergarden: JobDefinitionList = self.easy_client.export_jobs(
-            job_id_list_for_export
+        jobs_on_beer_garden_ids = list(map(id_getter, jobs_actually_on_beer_garden))
+        jobs_exported_from_beergarden: List[Job] = self.easy_client.export_jobs(
+            jobs_on_beer_garden_ids
         )
-
-        assert isinstance(jobs_exported_from_beergarden, JobDefinitionList)
-
-        exported_jobs = jobs_exported_from_beergarden.jobs
-
-        # check that the Jobs we got back are as we expect
-        assert (
-            isinstance(exported_jobs, list) and len(exported_jobs) == export_job_count
-        )
-        assert all(map(lambda x: isinstance(x, Job), exported_jobs))
 
         # now verify all names match, indicating up to ids, they are all the same Jobs
         assert (
-            set(map(name_getter, jobs_on_beer_garden_lifted))
-            >= set(map(name_getter, exported_jobs))
-            >= set(map(name_getter, jobs_on_beer_garden_lifted))
+            set(map(name_getter, jobs_actually_on_beer_garden))
+            >= set(map(name_getter, jobs_exported_from_beergarden))
+            >= set(map(name_getter, jobs_actually_on_beer_garden))
         )
         assert (
-            set(map(name_getter, jobs_to_be_created))
-            >= set(map(name_getter, exported_jobs))
-            >= set(map(name_getter, jobs_to_be_created))
-        )
-        assert (
-            set(map(name_getter, jobs_on_beer_garden_lifted))
-            >= set(map(name_getter, jobs_to_be_created))
-            >= set(map(name_getter, jobs_on_beer_garden_lifted))
+            set(map(name_getter, jobs_to_be_created_on_bg))
+            >= set(map(name_getter, jobs_exported_from_beergarden))
+            >= set(map(name_getter, jobs_to_be_created_on_bg))
         )
 
     def test_import_interval_jobs(self, system_spec):
         """Test Job import functionality."""
         import_job_count = 5
-        name_getter: Callable[[Job], str] = attrgetter("name")
-
         trigger = system_spec[0]
         req_template = system_spec[1]
 
-        base_jobs_list = [
-            self.create_interval_job(idx, trigger, req_template)
-            for idx in range(import_job_count)
-        ]
-        job_definition_list = JobDefinitionList(jobs=base_jobs_list)
+        job_definition_list = self.make_jobs(import_job_count, trigger, req_template)
         job_definitions_imported_to_beergarden = self.easy_client.import_jobs(
             job_definition_list
         )
 
-        assert isinstance(job_definitions_imported_to_beergarden, JobIDList)
-        assert len(job_definitions_imported_to_beergarden.ids) == import_job_count
-
         # we know we've been returned a type that says the jobs are imported, but
         # we need the actual Jobs that were created, so we search for them on the server
-        jobs_on_beer_garden = [
-            self.easy_client.find_jobs(name=job_name)
-            for job_name in map(name_getter, base_jobs_list)
-        ]
-
-        # lift them because they're lists
-        assert all(map(lambda x: isinstance(x, list), jobs_on_beer_garden))
-
-        jobs_on_beer_garden_lifted = list(map(lambda x: x[0], jobs_on_beer_garden))
-
-        assert all(
-            map(
-                lambda x: x is not None and isinstance(x, Job),
-                jobs_on_beer_garden_lifted,
-            )
+        jobs_on_beer_garden = self.find_jobs_on_bg_from_id_list(
+            job_definitions_imported_to_beergarden
         )
 
         # now that we have the Jobs that are actually on BG, verify they are the same
         # as those we imported (i.e. their names match)
         assert (
-            set(map(name_getter, base_jobs_list))
-            >= set(map(name_getter, jobs_on_beer_garden_lifted))
-            >= set(map(name_getter, base_jobs_list))
+            set(map(name_getter, job_definition_list))
+            >= set(map(name_getter, jobs_on_beer_garden))
+            >= set(map(name_getter, job_definition_list))
         )
 
+    def test_imported_jobs_assigned_next_run_time(self, system_spec):
+        """Test the scheduler assigns a next run time to imported jobs."""
+        import_job_count = 5
+        scheduler_wait_time_base = 0.01  # brief pause
+        trigger = system_spec[0]
+        req_template = system_spec[1]
+
+        job_definition_list = self.make_jobs(import_job_count, trigger, req_template)
+        job_definitions_imported_to_beergarden = self.easy_client.import_jobs(
+            job_definition_list
+        )
+
+        # wait for scheduler to do its thing
+        sleep(scheduler_wait_time_base * import_job_count)
+
+        jobs_on_beer_garden = self.find_jobs_on_bg_from_id_list(
+            job_definitions_imported_to_beergarden
+        )
+
+        def _is_not_none(x: Optional[Any]):
+            return x is not None
+
+        assert all(
+            map(_is_not_none, map(attrgetter("next_run_time"), jobs_on_beer_garden))
+        )
+
+    def create_jobs_on_bg(self, job_list: List[Job]):
+        return list(map(self.easy_client.create_job, job_list))  # noqa
+
+    def find_jobs_on_bg_from_job_list(
+        self, field: str, job_list: List[Job]
+    ) -> List[Job]:
+        return list(
+            map(
+                lambda x: x[0],
+                [
+                    self.easy_client.find_jobs(**{field: job_attr})
+                    for job_attr in map(attrgetter(field), job_list)
+                ],
+            )
+        )
+
+    def find_jobs_on_bg_from_id_list(self, job_id_list: List[str]) -> List[Job]:
+        return list(
+            map(
+                lambda x: x[0],
+                [self.easy_client.find_jobs(id=id_) for id_ in job_id_list],
+            )
+        )
+
+    @classmethod
+    def make_jobs(
+        cls, count: int, trigger: IntervalTrigger, request_template: RequestTemplate
+    ) -> List[Job]:
+        return [
+            cls.make_interval_job(idx + 1, trigger, request_template)
+            for idx in range(count)
+        ]
+
     @staticmethod
-    def create_interval_job(
+    def make_interval_job(
         idx: int, trigger: IntervalTrigger, request_template: RequestTemplate
-    ):
+    ) -> Job:
         """Create a Job from parameters."""
         request_template_commented = copy.deepcopy(request_template)
 
