407f077a6b2f875b7e4aa919204078566d9bb8af
Jake Rosss <jake.q.ross@gmail.com>
2021-09-13 16:08:28 +0000

    Linting, again

--- src/app/test/garden_test.py
+++ src/app/test/garden_test.py
@@ -2,9 +2,7 @@
 import pytest
 from brewtils.models import Garden as BrewtilsGarden
 from brewtils.models import System as BrewtilsSystem
-from brewtils.specification import _CONNECTION_SPEC
 from mongoengine import DoesNotExist, connect
-from yapconf import YapconfSpec
 
 from beer_garden import config
 from beer_garden.db.mongo.models import Garden, System
