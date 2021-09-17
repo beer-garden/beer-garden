2632f191884070fb6f29d67d6c614668bea497d3
Scott Taubman <86254668+scott-taubman@users.noreply.github.com>
2021-09-17 13:00:57 -0400

    #978 - More unit test fun

--- src/app/test/events/handlers_test.py
+++ src/app/test/events/handlers_test.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from mock import Mock
+
+import beer_garden
 from beer_garden.events.handlers import garden_callbacks
+
 
 class TestHandlers:
     def test_garden_callbacks_no_mangle(self, monkeypatch, bg_event):
@@ -9,8 +12,19 @@ class TestHandlers:
         def mangle(event):
             event = "mangled"
 
-        beer_garden.events.handlers.event_handlers = Mock()
-        for handler in beer_garden.events.handlers.event_handlers:
+        beer_garden.application = Mock()
+
+        for handler in [
+            beer_garden.garden,
+            beer_garden.plugin,
+            beer_garden.requests,
+            beer_garden.router,
+            beer_garden.systems,
+            beer_garden.scheduler,
+            beer_garden.log,
+            beer_garden.files,
+            beer_garden.local_plugins.manager,
+        ]:
             monkeypatch.setattr(handler, "handle_event", mangle)
 
         garden_callbacks(bg_event)
