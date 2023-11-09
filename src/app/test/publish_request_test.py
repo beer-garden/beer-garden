5e8c37d5088583dc4640b3af2a17228bb45950d2
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2023-11-09 17:00:28 +0000

    Fixed systems

--- src/app/test/publish_request_test.py
+++ src/app/test/publish_request_test.py
@@ -33,7 +33,7 @@ def command_topic_any():
 @pytest.fixture
 def localgarden_system(command_topic_one, command_topic_two, command_topic_one_and_two, command_topic_any):
     return System(
-            name="localsystem", version="1.2.3", namespace="localgarden", local=True,
+            name="localsystem", version="1.2.3", namespace="localgarden", local=True, instances=["default"],
             commands=[command_topic_one, command_topic_two, command_topic_one_and_two, command_topic_any]
         )
     
