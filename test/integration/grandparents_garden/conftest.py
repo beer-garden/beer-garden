e066e7138208f14cc2912ba538fd03ff111039c6
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2023-11-20 15:25:44 +0000

    Adding first test

---
+++ test/integration/grandparents_garden/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+from brewtils import get_easy_client
+from brewtils.schema_parser import SchemaParser
+
+try:
+    from ..helper import RequestGenerator, setup_easy_client
+except (ImportError, ValueError):
+    from helper import RequestGenerator, setup_easy_client
+
+
+@pytest.fixture(scope="class")
+def request_generator(request, system_spec):
+    request.cls.request_generator = RequestGenerator(**system_spec)
+
+@pytest.fixture(scope="class")
+def child_easy_client(request):
+    request.cls.child_easy_client = get_easy_client(
+        bg_host="localhost", bg_port=2357, ssl_enabled=False
+    )
+    return request.cls.child_easy_client
+
+@pytest.fixture(scope="class")
+def parent_easy_client(request):
+    request.cls.parent_easy_client = get_easy_client(
+        bg_host="localhost", bg_port=2347, ssl_enabled=False
+    )
+    return request.cls.child_easy_client
+
+@pytest.fixture(scope="class")
+def grand_parent_easy_client(request):
+    request.cls.grand_parent_easy_client = get_easy_client()
+    return request.cls.grand_parent_easy_client
+
+
+@pytest.fixture(scope="class")
+def parser(request):
+    request.cls.parser = SchemaParser()
+    return request.cls.parser
---
+++ test/integration/grandparents_garden/setup/garden_setup_test.py
@@ -0,0 +1,131 @@
+from time import sleep
+
+import pytest
+from brewtils.models import PatchOperation
+
+try:
+    from helper import wait_for_response
+    from helper.assertion import assert_successful_request
+except (ImportError, ValueError):
+    from ...helper import wait_for_response
+    from ...helper.assertion import assert_successful_request
+
+
+@pytest.fixture(scope="class")
+def system_spec():
+    return {
+        "namespace": "child",
+        "system": "echo",
+        "system_version": "3.0.0.dev0",
+        "instance_name": "default",
+        "command": "say",
+    }
+
+
+@pytest.mark.usefixtures(
+    "parser", "child_easy_client", "parent_easy_client", "grand_parent_easy_client", "request_generator"
+)
+class TestGardenSetup(object):
+    child_garden_name = "child"
+
+    def test_garden_auto_register_successful(self):
+        response = self.grand_parent_easy_client.client.session.get(
+            self.easy_client.client.base_url + "api/v1/gardens/"
+        )
+
+        gardens = self.parser.parse_garden(response.json(), many=True)
+
+        print(gardens)
+        assert len(gardens) == 2
+
+        for garden in gardens:
+            if garden.name == 'parent':
+                assert len(garden.children) == 1
+
+    # def test_update_garden_connection_info(self):
+    #     response = self.easy_client.client.session.get(
+    #         self.easy_client.client.base_url + "api/v1/gardens/"
+    #     )
+    #     gardens = self.parser.parse_garden(response.json(), many=True)
+
+    #     child_garden = None
+    #     for garden in gardens:
+    #         if garden.name == self.child_garden_name:
+    #             child_garden = garden
+    #             break
+
+    #     child_garden.connection_type = "HTTP"
+    #     child_garden.connection_params = {
+    #         "http": {"host": "beer-garden-child", "port": 2337, "ssl": False}
+    #     }
+
+    #     patch = PatchOperation(
+    #         operation="config",
+    #         path="",
+    #         value=self.parser.serialize_garden(child_garden, to_string=False),
+    #     )
+
+    #     payload = self.parser.serialize_patch(patch)
+
+    #     print(payload)
+    #     response = self.easy_client.client.session.patch(
+    #         self.easy_client.client.base_url
+    #         + "api/v1/gardens/"
+    #         + self.child_garden_name,
+    #         data=payload,
+    #         headers=self.easy_client.client.JSON_HEADERS,
+    #     )
+
+    #     assert response.ok
+
+    # def test_child_systems_register_successful(self):
+    #     systems = self.easy_client.find_systems()
+
+    #     namespaces = dict()
+
+    #     for system in systems:
+    #         if system.namespace not in namespaces.keys():
+    #             namespaces[system.namespace] = 1
+    #         else:
+    #             namespaces[system.namespace] += 1
+
+    #     print(namespaces)
+    #     assert (
+    #         self.child_garden_name in namespaces.keys()
+    #         and namespaces[self.child_garden_name] > 0
+    #     )
+
+    # def test_child_request_from_parent(self):
+    #     request = self.request_generator.generate_request(
+    #         parameters={"message": "test_string", "loud": True}
+    #     )
+    #     response = wait_for_response(self.easy_client, request)
+    #     assert_successful_request(response, output="test_string!!!!!!!!!")
+
+    # def test_child_request_from_child(self):
+    #     request = self.request_generator.generate_request(
+    #         parameters={"message": "test_string", "loud": True}
+    #     )
+    #     response = wait_for_response(self.child_easy_client, request)
+    #     assert_successful_request(response, output="test_string!!!!!!!!!")
+
+    # def test_verify_requests(self):
+    #     sleep(0.5)  # TODO: it is ridiculous that this is necessary
+    #     orig_requests_len = len(self.easy_client.find_requests())
+
+    #     new_request_clients = [self.easy_client, self.child_easy_client]
+
+    #     for client in new_request_clients:
+    #         new_request = wait_for_response(
+    #             client,
+    #             self.request_generator.generate_request(
+    #                 parameters={"message": "test_string", "loud": True}
+    #             ),
+    #         )
+    #         assert new_request.status == "SUCCESS"
+
+    #     new_requests_len = len(self.easy_client.find_requests())
+
+    #     assert new_requests_len == orig_requests_len + len(new_request_clients)
+
+    # TODO Add wait test
