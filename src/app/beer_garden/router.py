ae4b79f861f40b15c470f8b121596deea4b34318
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2023-12-29 07:28:55 -0500

    Fixed issue where Start spawned multiple processes (#1437)

--- src/app/beer_garden/router.py
+++ src/app/beer_garden/router.py
@@ -115,6 +115,7 @@ route_functions = {
     "INSTANCE_HEARTBEAT": beer_garden.plugin.heartbeat,
     "INSTANCE_INITIALIZE": beer_garden.plugin.initialize,
     "INSTANCE_START": beer_garden.plugin.start,
+    "INSTANCE_RESTART": beer_garden.plugin.restart,
     "INSTANCE_STOP": beer_garden.plugin.stop,
     "INSTANCE_LOGS": beer_garden.plugin.read_logs,
     "JOB_CREATE": beer_garden.scheduler.create_job,
