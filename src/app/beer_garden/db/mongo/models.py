c9f3cc1ec61a912ef7783d23256b9e106f56d680
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-01-02 15:29:55 -0500

    system logging

--- src/app/beer_garden/db/mongo/models.py
+++ src/app/beer_garden/db/mongo/models.py
@@ -832,7 +832,11 @@ class Garden(MongoModel, Document):
         if Garden.objects(name=self.name).count() > 0:
             old_garden = Garden.objects.get(name=self.name)
             for system in old_garden.systems:
-                logger.error(system)
+                logger.error(f"Old System === {system}")
+
+        for system in self.systems:
+                logger.error(f"New System === {system}")
+
 
         #our_namespaces = set(self.namespaces).union(
         #    set(map(attrgetter("namespace"), self.systems))
