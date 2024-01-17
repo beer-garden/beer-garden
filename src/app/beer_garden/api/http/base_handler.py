1f40962e7a4ea8396a5f80720b914947f118bc31
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-01-17 08:56:23 -0500

    sorting

--- src/app/beer_garden/api/http/base_handler.py
+++ src/app/beer_garden/api/http/base_handler.py
@@ -6,6 +6,18 @@ import re
 import socket
 from typing import Type, Union
 
+from brewtils.errors import (
+    AuthorizationRequired,
+    ConflictError,
+    ModelError,
+    ModelValidationError,
+    NotFoundError,
+    RequestForbidden,
+    RequestPublishException,
+    TimeoutExceededError,
+    WaitExceededError,
+)
+
 from marshmallow import Schema
 from marshmallow.exceptions import ValidationError as MarshmallowValidationError
 from mongoengine.errors import DoesNotExist, NotUniqueError
@@ -23,17 +35,6 @@ from beer_garden.errors import (
     NotUniqueException,
     RoutingException,
     RoutingRequestException,
-)
-from brewtils.errors import (
-    AuthorizationRequired,
-    ConflictError,
-    ModelError,
-    ModelValidationError,
-    NotFoundError,
-    RequestForbidden,
-    RequestPublishException,
-    TimeoutExceededError,
-    WaitExceededError,
 )
 
 
