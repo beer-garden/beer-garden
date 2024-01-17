8a967c5748b2e4035f9e3411f340dff4cebbf464
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-01-17 09:27:54 -0500

    change logic

--- src/app/beer_garden/api/http/base_handler.py
+++ src/app/beer_garden/api/http/base_handler.py
@@ -48,10 +48,11 @@ async def event_wait(evt, timeout):
 
 async def future_wait(future, timeout):
     """Helper method to add a timeout to an asyncio wait"""
-    try:
-        await asyncio.wait_for(future, timeout)
-    except asyncio.TimeoutError:
-        raise TimeoutExceededError("Timeout exceeded")
+    
+    await asyncio.wait(future, timeout=timeout)
+
+    if not future.done():
+        future.set_exception(TimeoutExceededError("Timeout exceeded"))
     return future
 
 
