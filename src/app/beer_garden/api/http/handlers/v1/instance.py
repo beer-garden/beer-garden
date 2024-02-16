f7d5fdedac3733b293937192505869f83ed0839e
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-02-16 13:36:10 -0500

    Support Read Logs as Routable (#1453)

--- src/app/beer_garden/api/http/handlers/v1/instance.py
+++ src/app/beer_garden/api/http/handlers/v1/instance.py
@@ -246,9 +246,9 @@ class InstanceLogAPI(AuthorizationHandler):
 
         response = await self._generate_get_response(instance_id, start_line, end_line)
 
-        self.set_header("request_id", response["id"])
+        self.set_header("request_id", response.id)
         self.set_header("Content-Type", "text/plain; charset=UTF-8")
-        self.write(response["output"])
+        self.write(response.output)
 
     async def _generate_get_response(self, instance_id, start_line, end_line):
         wait_future = Future()
@@ -256,8 +256,8 @@ class InstanceLogAPI(AuthorizationHandler):
         response = await self.client(
             Operation(
                 operation_type="INSTANCE_LOGS",
-                args=[instance_id],
                 kwargs={
+                    "instance_id": instance_id,
                     "wait_event": wait_future,
                     "start_line": start_line,
                     "end_line": end_line,
@@ -276,8 +276,8 @@ class InstanceLogAPI(AuthorizationHandler):
 
         response = wait_future.result()
 
-        if response["status"] == "ERROR":
-            raise RequestProcessingError(response["output"])
+        if response.status == "ERROR":
+            raise RequestProcessingError(response.output)
 
         return response
 
