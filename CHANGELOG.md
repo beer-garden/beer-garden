568a01d51c827c36b8a1e0a298afb212bdfa124e
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-03-27 11:48:37 -0400

    Add configuration for Garden Latency calculations (#1488)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,7 @@ TBD
 - Utilized the TTL for In_Progress requests to set Expiration windows for Requests on PIKA, to handle backups of requests that Beer Garden has already cancelled.
 - Optimized UI Rest calls to load only Local Garden with Children
 - Updated how STOMP URL preview is formatted on Garden Admin page
+- Added flag to enable the collection of Garden Latency metrics based off Request event times
 
 # 3.24.4
 
--- src/app/beer_garden/config.py
+++ src/app/beer_garden/config.py
@@ -1265,6 +1265,11 @@ _LOG_SPEC = {
 _METRICS_SPEC = {
     "type": "dict",
     "items": {
+        "garden_latency_metrics_enabled": {
+            "type": "bool",
+            "description": "Enable collection of Garden latency metrics based off Requests",
+            "default": False,
+        },
         "prometheus": {
             "type": "dict",
             "items": {
@@ -1289,7 +1294,7 @@ _METRICS_SPEC = {
                     "required": False,
                 },
             },
-        }
+        },
     },
 }
 
