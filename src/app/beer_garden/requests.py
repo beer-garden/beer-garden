4170d78838be8b3be881cd7e9fb1088f4982eb78
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-03-27 14:03:13 +0000

    sort imports

--- src/app/beer_garden/requests.py
+++ src/app/beer_garden/requests.py
@@ -20,15 +20,7 @@ from typing import Dict, List, Sequence, Union
 
 import six
 import urllib3
-from requests import Session
 
-import beer_garden.config as config
-import beer_garden.db.api as db
-import beer_garden.queue.api as queue
-from beer_garden.db.mongo.models import RawFile
-from beer_garden.errors import NotUniqueException, ShutdownError
-from beer_garden.events import publish_event
-from beer_garden.metrics import request_completed, request_created, request_started
 from brewtils.choices import parse
 from brewtils.errors import (
     ConflictError,
@@ -47,6 +39,15 @@ from brewtils.models import (
     System,
 )
 from brewtils.pika import PERSISTENT_DELIVERY_MODE
+from requests import Session
+
+import beer_garden.config as config
+import beer_garden.db.api as db
+import beer_garden.queue.api as queue
+from beer_garden.db.mongo.models import RawFile
+from beer_garden.errors import NotUniqueException, ShutdownError
+from beer_garden.events import publish_event
+from beer_garden.metrics import request_completed, request_created, request_started
 
 logger = logging.getLogger(__name__)
 
