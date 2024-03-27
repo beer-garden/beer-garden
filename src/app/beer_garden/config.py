58bd6e192106c9dc338dffc806608b04afcddd3d
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-03-27 13:48:14 +0000

    Add configuration for Garden Latency caculations

--- src/app/beer_garden/config.py
+++ src/app/beer_garden/config.py
@@ -1260,6 +1260,11 @@ _LOG_SPEC = {
 _METRICS_SPEC = {
     "type": "dict",
     "items": {
+        "garden_latency_metrics_enabled": {
+            "type": "bool",
+            "description": "Enable collection of Garden latency metrics based off Requests",
+            "default": False,
+        }
         "prometheus": {
             "type": "dict",
             "items": {
