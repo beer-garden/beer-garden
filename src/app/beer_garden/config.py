7baef457927bd1d01eb8f08cd3875c637be63ddd
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-04-05 17:01:56 +0000

    Drop metrics config fields

--- src/app/beer_garden/config.py
+++ src/app/beer_garden/config.py
@@ -1273,19 +1273,6 @@ _LOG_SPEC = {
 _METRICS_SPEC = {
     "type": "dict",
     "items": {
-        "garden_latency_metrics_enabled": {
-            "type": "bool",
-            "description": "Enable collection of Garden latency metrics based off Requests",
-            "default": False,
-        },
-        "garden_latency_metrics_cache_window": {
-            "type": "int",
-            "default": 5,
-            "description": (
-                "Number of minutes to writing cache to "
-                "database (negative number for never cache)"
-            ),
-        },
         "prometheus": {
             "type": "dict",
             "items": {
