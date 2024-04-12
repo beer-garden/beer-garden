573ab3fed7bc0ddc1f70430f80985d1b7871cf90
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-04-12 18:33:52 +0000

    test

--- test/integration/local_plugins/plugins/echo_test.py
+++ test/integration/local_plugins/plugins/echo_test.py
@@ -31,11 +31,11 @@ class TestEcho(object):
  
                 stopped_instance = self.easy_client.client.patch_instance(instance.id, SchemaParser.serialize_patch(PatchOperation(operation="stop")))
                 assert stopped_instance.ok
-                assert SchemaParser.serialize_instance(stopped_instance).status == "STOPPED"
+                assert stopped_instance.status == "STOPPED"
 
                 start_instance = self.easy_client.client.patch_instance(instance.id, SchemaParser.serialize_patch(PatchOperation(operation="start")))
                 assert start_instance.ok
-                assert SchemaParser.serialize_instance(start_instance).status == "RUNNING"
+                assert start_instance.status == "RUNNING"
                 
                 test_ran = True
         
