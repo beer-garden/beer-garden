9b29d2e4cfa668062b9c738655652e1f7043dbc8
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-04-12 17:52:09 +0000

    test

--- test/integration/local_plugins/plugins/echo_test.py
+++ test/integration/local_plugins/plugins/echo_test.py
@@ -20,8 +20,8 @@ def system_spec():
 @pytest.mark.usefixtures("easy_client", "request_generator")
 class TestEcho(object):
 
-    def test_stop_start(self):
-        system = self.easy_client.find_unique_system(name=system_spec()["system"], version=system_spec()["system_version"])
+    def test_stop_start(self, system_spec):
+        system = self.easy_client.find_unique_system(name=system_spec["system"], version=system_spec["system_version"])
         test_ran = False
         for instance in system.instances:
             if instance.name == self.system_spec.instance_name:
