f6e56ffeaefc9a39fb978e4aff4228d9bab4f910
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-04-12 17:23:32 +0000

    echo restart testing

--- test/integration/local_plugins/plugins/echo_test.py
+++ test/integration/local_plugins/plugins/echo_test.py
@@ -6,7 +6,8 @@ try:
 except (ImportError, ValueError):
     from ...helper import wait_for_response
     from ...helper.assertion import assert_successful_request, assert_validation_error
-
+from tornado.httpclient import HTTPRequest
+import json
 
 @pytest.fixture(scope="class")
 def system_spec():
@@ -20,6 +21,34 @@ def system_spec():
 
 @pytest.mark.usefixtures("easy_client", "request_generator")
 class TestEcho(object):
+
+    def test_stop_start(self, http_client):
+        system = self.easy_client.find_unique_system(name=self.system_spec.system, version=self.system_spec.system_version)
+        test_ran = False
+        for instance in system.instances:
+            if instance.name == self.system_spec.instance_name:
+                url = f"localhost:2337/api/v1/instances/{instance.id}"
+                headers = {"Content-Type": "application/json"}
+                stop_patch_body = {"operations": [{"operation": "stop"}]}
+                start_patch_body = {"operations": [{"operation": "start"}]}
+
+                stop_request = HTTPRequest(
+                    url, method="PATCH", headers=headers, body=json.dumps(stop_patch_body)
+                )
+                stop_response = yield http_client.fetch(stop_request)
+
+                assert stop_response.code == 200
+
+                start_request = HTTPRequest(
+                    url, method="PATCH", headers=headers, body=json.dumps(start_patch_body)
+                )
+                start_response = yield http_client.fetch(start_request)
+
+                assert start_response.code == 200
+                test_ran = True
+        
+        assert test_ran
+
     def test_say_custom_string_and_loud(self):
         request = self.request_generator.generate_request(
             parameters={"message": "test_string", "loud": True}
