3199b4288b0020bb1e285c104024480595a66976
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-04-15 06:22:27 -0400

    move testing for start stop

--- test/integration/local_plugins/plugins/echo_test.py
+++ test/integration/local_plugins/plugins/echo_test.py
@@ -1,6 +1,4 @@
 import pytest
-from brewtils.models import PatchOperation
-from brewtils.schema_parser import SchemaParser
 
 try:
     from helper import wait_for_response
@@ -22,32 +20,6 @@ def system_spec():
 
 @pytest.mark.usefixtures("easy_client", "request_generator")
 class TestEcho(object):
-
-    def test_stop_start(self, system_spec):
-        test_ran = False
-
-        system = self.easy_client.find_unique_system(
-            name=system_spec["system"], version=system_spec["system_version"]
-        )
-        for instance in system.instances:
-            if instance.name == system_spec["instance_name"]:
-                assert instance.status == "RUNNING"
-
-                stopped_instance = self.easy_client.client.patch_instance(
-                    instance.id,
-                    SchemaParser.serialize_patch(PatchOperation(operation="stop")),
-                )
-                assert stopped_instance.ok
-
-                start_instance = self.easy_client.client.patch_instance(
-                    instance.id,
-                    SchemaParser.serialize_patch(PatchOperation(operation="start")),
-                )
-                assert start_instance.ok
-
-                test_ran = True
-
-        assert test_ran
 
     def test_say_custom_string_and_loud(self):
         request = self.request_generator.generate_request(
