3934353057465cb7d062cec7a621d2ce9c14b584
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-07-24 15:33:04 -0400

    Updating Testing

--- test/regression_test/regression_test.sh
+++ test/regression_test/regression_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ## All of the versions to test
-versions=("3.23.0","3.23.1","3.24.0","3.24.1","3.24.2","3.24.3","3.24.4","3.25.0","3.25.1","3.26.0","3.26.1","3.26.2","3.26.3","3.26.4")
+versions=("3.26.4","3.26.3","3.26.2","3.26.1","3.26.0","3.25.1","3.25.0","3.24.4","3.24.3","3.24.2","3.24.1","3.24.0","3.23.1","3.23.0","3.22.0","3.21.0","3.20.0")
+# versions=("3.23.0")
 
 ## now loop through the above array
 for version in "${versions[@]}"
@@ -15,7 +16,7 @@ do
 	docker image tag bgio/beer-garden:$version bgio/beer-garden:regression
 	
 	# Start BG
-	docker compose up -d mongodb rabbitmq beer-garden-parent beer-garden-child
+	docker-compose up -d mongodb rabbitmq beer-garden-parent beer-garden-child
 	
 	# Give BG a minute to start up
     echo "Wait 60 seconds"
@@ -23,7 +24,7 @@ do
 	
 	# Pre-Load database
     echo "Load Database"
-	docker compose up jmeter-loader
+	docker-compose up jmeter-loader | tee $version-jmeter-preload-database.log &
 
 	# Give BG a minute to process requests
     echo "Wait 120 seconds"
@@ -31,9 +32,10 @@ do
 	
 	# Run testing
     echo "Run Tests"
-	docker compose up jmeter-testing-requests | tee $version-jmeter-request.log &
-	docker compose up jmeter-testing-ui | tee $version-jmeter-ui.log &
-	docker compose up jmeter-testing-start-stop | tee $version-jmeter-start-stop.log &
+	docker-compose up jmeter-testing-requests | tee $version-jmeter-request.log &
+	docker-compose up jmeter-testing-events | tee $version-jmeter-events.log &
+	docker-compose up jmeter-testing-ui | tee $version-jmeter-ui.log &
+	docker-compose up jmeter-testing-start-stop | tee $version-jmeter-start-stop.log &
 	wait
 	
 	# Evaluate the output files to grab metrics
