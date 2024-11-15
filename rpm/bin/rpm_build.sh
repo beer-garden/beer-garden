06b5f8d8da97ad8032d32b346045fc20d7688b2f
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2024-11-15 09:03:33 -0500

    fixing build script

--- rpm/bin/rpm_build.sh
+++ rpm/bin/rpm_build.sh
@@ -45,7 +45,7 @@ done
 if [ -z "$RELEASE" ]; then
   echo "RELEASE not specified, using 7"
   RELEASE="7"
-elif [[ "$RELEASE" != "7" ] && [ "$RELEASE" != "9" ]]; then
+elif [ "$RELEASE" != "7" ] && [ "$RELEASE" != "9" ]; then
   echo "Unsupported RELEASE: ${RELEASE}"
   echo "Supported releases are 7 and 9"
   exit 1
