6e4b84a6b2f09b9f20f3a2445944321174bf7669
1maple1 <160027655+1maple1@users.noreply.github.com>
2025-01-13 12:46:13 +0000

    linting

--- src/app/beer_garden/api/http/authentication/login_handlers/certificate.py
+++ src/app/beer_garden/api/http/authentication/login_handlers/certificate.py
@@ -45,9 +45,9 @@ class CertificateLoginHandler(BaseLoginHandler):
                     user = get_user(username=username)
 
                     authenticated_user = user
-                    authenticated_user.metadata["last_authentication"] = (
-                        datetime.now(timezone.utc).timestamp()
-                    )
+                    authenticated_user.metadata["last_authentication"] = datetime.now(
+                        timezone.utc
+                    ).timestamp()
                     authenticated_user = update_user(user=authenticated_user)
 
                 except DoesNotExist:
