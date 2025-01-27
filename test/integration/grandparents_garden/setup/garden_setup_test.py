10a55a1ebed155b7bf80738a26300afc9b18babf
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-01-27 10:46:56 -0500

    fixing pipeline order

--- test/integration/grandparents_garden/setup/garden_setup_test.py
+++ test/integration/grandparents_garden/setup/garden_setup_test.py
@@ -121,7 +121,7 @@ class TestGardenSetup(object):
 
                 # Older gardens will not have receiving connections present
                 if len(garden.receiving_connections) > 0:
-                    for connection in garden.publishing_connections:
+                    for connection in garden.receiving_connections:
                         if connection.api == "HTTP":
                             assert connection.status == "RECEIVING"
                         else:
