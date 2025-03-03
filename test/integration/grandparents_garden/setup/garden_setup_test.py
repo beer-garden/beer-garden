929b1ca781f6e0f753c1f5e3667a60506c3af416
jwfowl2 <160027655+1maple1@users.noreply.github.com>
2025-03-03 13:01:01 +0000

    Update integration test

--- test/integration/grandparents_garden/setup/garden_setup_test.py
+++ test/integration/grandparents_garden/setup/garden_setup_test.py
@@ -93,11 +93,11 @@ class TestGardenSetup(object):
                         assert connection.status == "NOT_CONFIGURED"
                 assert len(garden.receiving_connections) == 1
                 assert garden.receiving_connections[0].status == "RECEIVING"
-                assert len(garden.children) == 1
-                assert garden.children[0].name == "child"
+                assert len(garden.downstream) == 1
+                assert garden.downstream[0].name == "child"
 
             elif garden.name == "grandparent":
-                assert len(garden.children) == 1
+                assert len(garden.downstream) == 1
             else:
                 raise AssertionError()
 
@@ -123,10 +123,10 @@ class TestGardenSetup(object):
                 if len(garden.receiving_connections) > 0:
                     assert len(garden.receiving_connections) == 1
                     assert garden.receiving_connections[0].status == "RECEIVING"
-                    assert len(garden.children) == 0
+                    assert len(garden.downstream) == 0
 
             elif garden.name == "parent":
-                assert len(garden.children) == 1
+                assert len(garden.downstream) == 1
             else:
                 raise AssertionError()
 
@@ -152,12 +152,12 @@ class TestGardenSetup(object):
 
         for garden in gardens:
             print(garden)
-            assert not garden.has_parent
+            assert not garden.has_upstream
 
         for garden in gardens:
             if garden.name == "parent":
-                assert garden.children is not None
-                assert len(garden.children) == 1
+                assert garden.downstream is not None
+                assert len(garden.downstream) == 1
 
         assert len(gardens) == 2
 
