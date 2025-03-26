928d6724a78f8e4d054f17e87817b1c1ffa25dbb
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-03-26 13:29:06 -0400

    Update topic.py

--- src/app/beer_garden/topic.py
+++ src/app/beer_garden/topic.py
@@ -259,13 +259,12 @@ def subscriber_systems_validate(subscriber, systems, topic_name: str):
 
 
 def sync_garden_topics(garden_name: str = None):
-
     if garden_name is None:
         garden = get_garden(config.get("garden.name"))
     else:
         garden = get_garden(garden_name)
 
-    logger.info(f"Running Garden Topic Sync for {garden.name")
+    logger.info(f"Running Garden Topic Sync for {garden.name}")
     
     topics = get_all_topics()
 
