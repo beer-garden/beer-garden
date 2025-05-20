8c867e6508269fa1677d48d622bfb8dd1627c7de
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-05-20 14:31:53 +0000

    Removed commented out code

--- src/app/beer_garden/db/mongo/pruner.py
+++ src/app/beer_garden/db/mongo/pruner.py
@@ -429,10 +429,6 @@ def prune_outstanding_requests(outstanding_requests):
                 request.status = "CANCELED"
                 request.status_updated_at = datetime.now(timezone.utc)
                 request.save()
-                # serialized = MongoParser.serialize(request, to_string=True)
-                # parsed = SchemaParser.parse_request(
-                #     serialized, from_string=True, many=False
-                # )
 
                 parsed = to_brewtils(request)
 
