64cb81464860c22ef311268945e6ce4f4390921a
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-05-22 19:29:17 +0000

    Cleanup try catch

--- src/app/beer_garden/api/stomp/manager.py
+++ src/app/beer_garden/api/stomp/manager.py
@@ -210,31 +210,25 @@ class StompManager(BaseProcessor):
             logger.exception(f"Error executing callback for {event!r}: {ex}")
 
         if not event.error:
-            if event.name in [
-                Events.SYSTEM_CREATED.name,
-                Events.SYSTEM_UPDATED.name,
-                Events.GARDEN_SYNC.name,
-                Events.GARDEN_CONFIGURED.name,
-                Events.GARDEN_REMOVED.name,
-                Events.GARDEN_UPDATED.name,
-            ]:
-                try:
+            try:
+                if event.name in [
+                    Events.SYSTEM_CREATED.name,
+                    Events.SYSTEM_UPDATED.name,
+                    Events.GARDEN_SYNC.name,
+                    Events.GARDEN_CONFIGURED.name,
+                    Events.GARDEN_REMOVED.name,
+                    Events.GARDEN_UPDATED.name,
+                ]:
                     beer_garden.router.handle_event(deepcopy(event))
-                except Exception as ex:
-                    logger.exception(f"Error executing callback for {event!r}: {ex}")
 
-            elif event.name == Events.PLUGIN_LOGGER_FILE_CHANGE.name:
-                try:
+                elif event.name == Events.PLUGIN_LOGGER_FILE_CHANGE.name:
                     beer_garden.log.handle_event(deepcopy(event))
-                except Exception as ex:
-                    logger.exception(f"Error executing callback for {event!r}: {ex}")
 
-            elif event.name in [
-                Events.REQUEST_COMPLETED.name,
-                Events.REQUEST_CANCELED.name,
-                Events.GARDEN_STOPPED.name,
-            ]:
-                try:
+                elif event.name in [
+                    Events.REQUEST_COMPLETED.name,
+                    Events.REQUEST_CANCELED.name,
+                    Events.GARDEN_STOPPED.name,
+                ]:
                     beer_garden.requests.handle_wait_events(deepcopy(event))
-                except Exception as ex:
-                    logger.exception(f"Error executing callback for {event!r}: {ex}")
+            except Exception as ex:
+                logger.exception(f"Error executing callback for {event!r}: {ex}")
