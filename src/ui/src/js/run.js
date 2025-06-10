9a174f2d88dd78edf6944dda5cd58f9c64097199
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-06-10 14:47:51 +0000

    Lazy Load Gardens on Request View Page

--- src/ui/src/js/run.js
+++ src/ui/src/js/run.js
@@ -374,6 +374,7 @@ export default function appRun(
       GardenService.getGarden($rootScope.config.gardenName).then((response) => {
         $rootScope.garden = response.data;
         $rootScope.gardensResponse = response;
+        $rootScope.systems = $rootScope.garden.systems;
         return callback();
       },
         (response) => {
