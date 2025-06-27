a7d96b5ab9a5347fc681f94b39603da59fc96c7f
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-06-27 17:10:44 +0000

    fixed logging

--- src/app/beer_garden/db/mongo/models.py
+++ src/app/beer_garden/db/mongo/models.py
@@ -1281,7 +1281,7 @@ class Garden(MongoModel, Document):
         # if there's anything left over, delete those too; this could occur, e.g.,
         # if a child system deleted a particular version of a plugin and installed
         # another version of the same plugin
-        for key, bad_system_id in child_systems_already_known.items:
+        for bad_system_id, key in child_systems_already_known.items():
             logger.error(
                 f"Removing System with ID={str(bad_system_id)} because it "
                 f"matches no known system in child garden ({self.name}) or key {key}"
