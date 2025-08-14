19e429cf8205cc004fc6514a385d35f3b267fc76
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-08-14 10:43:54 +0000

    cleanup

--- src/app/beer_garden/requests.py
+++ src/app/beer_garden/requests.py
@@ -1101,7 +1101,6 @@ def handle_event_filter(event):
 
 def handle_event_rebroadcast(event_name, request):
 
-    metadata = {}
     # If no parent are set, we only want to publish to the UI events handler
     if not config.get("parent.stomp.enabled") and not config.get("parent.http.enabled"):
         publish(
