73c654ff857a701f4145f1ad512e1dc18391f0f0
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-08-22 13:56:37 +0000

    revert changes

--- src/app/beer_garden/publish_request.py
+++ src/app/beer_garden/publish_request.py
@@ -333,10 +333,6 @@ def process_publish_event(
                         event_request.command_type = command.command_type
                         event_request.is_event = True
 
-                        if not event_request.metadata:
-                            event_request.metadata = {}
-                        event_request.metadata["topic"] = topic.name
-
                         request_hash = (
                             f"{garden_name}.{system.namespace}."
                             f"{system.name}.{system.version}."
