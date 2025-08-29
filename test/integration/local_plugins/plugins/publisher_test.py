822269f468279f0bf6092b8462988653edada2d6
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-08-29 19:57:47 +0000

    Update test

--- test/integration/local_plugins/plugins/publisher_test.py
+++ test/integration/local_plugins/plugins/publisher_test.py
@@ -93,10 +93,11 @@ class TestPublish(object):
 
         completed_request = self.wait_for_request(request, 1)
 
+        assert len(completed_request.children) == 1
+        
         for child_request in completed_request.children:
             assert child_request.command == "subscribe_wildcard_topics"
 
-        assert len(completed_request.children) == 1
         self.easy_client.remove_topic(topic.id)
 
     def test_one_trigger(self):
