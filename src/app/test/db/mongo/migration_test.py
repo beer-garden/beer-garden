f1e9db99f5ea5d7369f204e0156b95e00489dbf7
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-08-29 12:47:01 +0000

    Adding test classes

--- src/app/test/db/mongo/migration_test.py
+++ src/app/test/db/mongo/migration_test.py
@@ -3,6 +3,7 @@ import copy
 from datetime import datetime
 
 import gridfs
+import pytest
 from bson.dbref import DBRef
 from mock import Mock, patch
 from mongoengine.connection import get_db
@@ -16,6 +17,14 @@ from beer_garden.db.mongo.migration import (  # ensure_roles,; ensure_users,
 from mongomock.gridfs import enable_gridfs_integration
 
 enable_gridfs_integration()
+
+
+@pytest.fixture(autouse=True)
+def drop():
+    yield
+    db = get_db()
+    for collection in db.list_collection_names():
+        db[collection].drop()
 
 
 class TestMigrationScript(object):
@@ -299,3 +308,39 @@ class TestMigrationScript(object):
             )
             assert post_migration.get("status") == db_request.get("status")
             assert post_migration.get("updated_at") is not None
+
+    @patch("mongoengine.connect", Mock())
+    @patch("mongoengine.register_connection", Mock())
+    def test_3_30_request_migration_no_target_garden(self, request_dict, ts_dt):
+
+        config._CONFIG = {"garden": {"name": "test_garden"}}
+
+        del request_dict["target_garden"]
+
+        db = get_db()
+        request_collection = db["request"]
+        request_collection.insert_one(request_dict)
+
+        ensure_v3_30_model_migration()
+
+        request = request_collection.find_one()
+
+        assert request["target_garden"] == "test_garden"
+
+    @patch("mongoengine.connect", Mock())
+    @patch("mongoengine.register_connection", Mock())
+    def test_3_30_request_migration_no_source_garden(self, request_dict, ts_dt):
+
+        config._CONFIG = {"garden": {"name": "test_garden"}}
+
+        del request_dict["source_garden"]
+
+        db = get_db()
+        request_collection = db["request"]
+        request_collection.insert_one(request_dict)
+
+        ensure_v3_30_model_migration()
+
+        request = request_collection.find_one()
+
+        assert request["source_garden"] == "test_garden"
