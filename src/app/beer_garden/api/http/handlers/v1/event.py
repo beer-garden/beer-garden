81a121b2e4887b56ae77a39f3765908962761d94
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-09-29 20:24:24 +0000

    Cleanup

--- src/app/beer_garden/api/http/handlers/v1/event.py
+++ src/app/beer_garden/api/http/handlers/v1/event.py
@@ -157,7 +157,7 @@ class EventSocket(WebSocketHandler):
                         else:
 
                             await listener.write_message(message)
-                except BaseException:
+                except Exception:
                     continue
 
     @classmethod
