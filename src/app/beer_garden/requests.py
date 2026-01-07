6d0729687b3deaea2768ab70fb1f348ab7d610be
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-01-07 13:50:58 +0000

    stash

--- src/app/beer_garden/requests.py
+++ src/app/beer_garden/requests.py
@@ -574,6 +574,14 @@ class RequestValidator(object):
                 % (parameter.key, parameter.type)
             )
 
+def get_request_children(request):
+
+    request.children = db.query(Request, filter_params={"parent": request})
+
+    if request.children:
+        for child in request.children:
+            child = get_request_children(child)
+    return request
 
 def get_request(request_id: str = None, request: Request = None) -> Request:
     """Retrieve an individual Request
@@ -587,7 +595,8 @@ def get_request(request_id: str = None, request: Request = None) -> Request:
 
     """
     request = request or db.query_unique(Request, id=request_id, raise_missing=True)
-    request.children = db.query(Request, filter_params={"parent": request})
+    get_request_children(request)
+    # request.children = db.query(Request, filter_params={"parent": request})
 
     return request
 
