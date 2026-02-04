02d95d33192b85ae2002ca35597d147e27d209f4
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-02-04 17:34:50 +0000

    expanding ignore list

--- src/react/eslint.config.ts
+++ src/react/eslint.config.ts
@@ -25,12 +25,16 @@ export default defineConfig(
     },
     rules: {
       // Add or override specific rules here
+
+      // Disabled rules that check for any usage
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-redundant-type-constituents': 'off',
+      
       '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
