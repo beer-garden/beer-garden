c684a9af033eee3b8a2e391249468e32877cb814
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-02-13 16:12:32 +0000

    formatting

--- src/react/src/services/util_service.test.tsx
+++ src/react/src/services/util_service.test.tsx
@@ -1,10 +1,11 @@
-import { expect, test } from 'vitest'
-import { CompareObjects } from './util_service.js'
+import { expect, test } from "vitest";
 
-test('Compare Equal Objects', () => {
-  expect(CompareObjects({ a: 1, b: 2 }, { a: 1, b: 2 })).toBe(true)
-})
+import { CompareObjects } from "./util_service.js";
 
-test('Compare Different Objects', () => {
-  expect(CompareObjects({ a: 1, b: 2 }, { a: 1, b: 3 })).toBe(false)
-})
+test("Compare Equal Objects", () => {
+  expect(CompareObjects({ a: 1, b: 2 }, { a: 1, b: 2 })).toBe(true);
+});
+
+test("Compare Different Objects", () => {
+  expect(CompareObjects({ a: 1, b: 2 }, { a: 1, b: 3 })).toBe(false);
+});
