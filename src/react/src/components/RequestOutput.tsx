b35dbd95d7b0c352f9b92a3cc30a07b0e27fed95
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-03-09 17:14:17 +0000

    Support large outputs and missing output type

--- src/react/src/components/RequestOutput.tsx
+++ src/react/src/components/RequestOutput.tsx
@@ -1,8 +1,27 @@
+import { Button } from "primereact/button";
 import { Skeleton } from "primereact/skeleton";
+import { useState } from "react";
 
 import { Request } from "../models/brewtils-types";
 
-function displautOutput(request: Request) {
+function largeOutputCheck(request: Request): boolean {
+  if (request.output_type === "JSON") {
+    try {
+      if (Object.keys(JSON.parse(request.output || "{}")).length > 1000) {
+        return false;
+      }
+    } catch {
+      // Pass
+    }
+  }
+  if (new Blob([request.output ?? ""]).size > 5000000) {
+    return false;
+  }
+
+  return true;
+}
+
+function formattedOutputData(request: Request) {
   if (request.output_type === "JSON") {
     let parsed_output = {};
     try {
@@ -20,23 +39,41 @@ function displautOutput(request: Request) {
     );
   }
 
-  if (request.output_type === "STRING") {
-    return <pre>{request.output}</pre>;
-  }
+  // Covers STRING, XML, JS, CSS
+  return (
+    <pre
+      style={{
+        whiteSpace: "pre-wrap",
+        overflowWrap: "break-word",
+        overflowX: "auto",
+      }}
+    >
+      {request.output}
+    </pre>
+  );
+}
 
-  if (request.output_type === "XML") {
-    return <pre>{request.output}</pre>;
-  }
+function displayOutput(request: Request) {
+  const [showOutput, setShowOutput] = useState(largeOutputCheck(request));
 
-  if (request.output_type === "JS") {
-    return <pre>{request.output}</pre>;
-  }
-
-  if (request.output_type === "CSS") {
-    return <pre>{request.output}</pre>;
-  }
-
-  return <div></div>;
+  return (
+    <div>
+      {!showOutput && (
+        <div>
+          <div>Output is too large</div>
+          <Button
+            label="Show Output"
+            severity="warning"
+            icon="pi pi-arrow-right"
+            iconPos="right"
+            data-testid="request-show-output"
+            onClick={() => setShowOutput(true)}
+          />
+        </div>
+      )}
+      {showOutput && formattedOutputData(request)}
+    </div>
+  );
 }
 
 function RequestOutput(request: Request) {
@@ -51,7 +88,7 @@ function RequestOutput(request: Request) {
     );
   }
 
-  return <div id="request-output">{displautOutput(request)}</div>;
+  return <div id="request-output">{displayOutput(request)}</div>;
 }
 
 export default RequestOutput;
