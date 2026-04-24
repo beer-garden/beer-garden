216649b38ef6051beb0315c03acef05345009a0b
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-04-24 18:32:51 +0000

    Add router filtering

--- src/app/beer_garden/router.py
+++ src/app/beer_garden/router.py
@@ -27,6 +27,7 @@ from brewtils import EasyClient
 from brewtils.models import Connection as BrewtilsConnection
 from brewtils.models import Events, Garden, Operation, System
 from mongoengine import DoesNotExist
+from packaging.version import parse
 
 # from packaging.version import parse
 from stomp.exception import ConnectFailedException
@@ -861,6 +862,14 @@ def _pre_forward(operation: Operation) -> Operation:
             operation.model.command_type = "INFO"
 
         beer_garden.files.forward_file(operation)
+
+        # Removed non legacy support values
+        if (
+            operation.model.target_garden not in gardens
+            or gardens[operation.model.target_garden].version is None
+            or parse(gardens[operation.model.target_garden].version) < parse("3.33.0")
+        ):
+            operation.kwargs.pop("choice_validation_enabled", None)
 
         # Clear parent before forwarding so the child doesn't freak out about an
         # unknown request
