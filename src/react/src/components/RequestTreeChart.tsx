a6ccf162c22ca0e82dbc52493f8afdf052c48022
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-05-13 15:31:07 -0400

    Add command info icons to request tree (#2161)
    * Add command info icons to request tree
    
    * formatting

--- src/react/src/components/RequestTreeChart.tsx
+++ src/react/src/components/RequestTreeChart.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "primereact/button";
 import { Column } from "primereact/column";
+import { Tooltip } from "primereact/tooltip";
 import { TreeTable } from "primereact/treetable";
 import { Link } from "react-router-dom";
 
@@ -16,6 +17,8 @@ function parseRequest(request: Request, config: Config) {
       id: request.id,
       command: request.command,
       command_display_name: request.command_display_name,
+      command_type: request.command_type,
+      topic: request?.metadata?._topic,
       status: request.status,
       namespace: request.namespace,
       system: request.system,
@@ -60,6 +63,54 @@ function RequestTreeChart(props: RequestTreeChartProps) {
 
   const rowClassName = (node: any) => {
     return { "p-highlight": node.data.id === props.currentRequestId };
+  };
+
+  const commandNameTemplate = (node: any) => {
+    return (
+      <div>
+        {node.data.topic && (
+          <>
+            <Tooltip
+              content={node.data.topic}
+              target={`#TOPIC_${node.data.id}`}
+            />
+            <FontAwesomeIcon
+              icon="envelope"
+              className="mr-2"
+              id={`TOPIC_${node.data.id}`}
+            />
+          </>
+        )}
+        <Tooltip
+          content={`${node.data.command_type} Command`}
+          target={`#ICON_${node.data.id}`}
+        />
+        {(node.data.command_type === undefined ||
+          node.data.command_type.length === 0 ||
+          node.data.command_type === "ACTION") && (
+          <FontAwesomeIcon
+            icon="a"
+            className="mr-2"
+            id={`ICON_${node.data.id}`}
+          />
+        )}
+        {node.data.command_type === "INFO" && (
+          <FontAwesomeIcon
+            icon="i"
+            className="mr-2"
+            id={`ICON_${node.data.id}`}
+          />
+        )}
+        {node.data.command_type === "TEMP" && (
+          <FontAwesomeIcon
+            icon="hourglass"
+            className="mr-2"
+            id={`ICON_${node.data.id}`}
+          />
+        )}
+        <span>{node.data.command}</span>
+      </div>
+    );
   };
 
   const actionTemplate = (node: any, config: Config) => {
@@ -146,7 +197,12 @@ function RequestTreeChart(props: RequestTreeChartProps) {
         rowClassName={rowClassName}
         tableStyle={{ minWidth: "50rem" }}
       >
-        <Column field="command" header="Command" expander></Column>
+        <Column
+          field="command"
+          header="Command"
+          expander
+          body={commandNameTemplate}
+        ></Column>
 
         <Column field="status" header="status"></Column>
         <Column field="namespace" header="Namespace"></Column>
