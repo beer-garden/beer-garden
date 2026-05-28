481b090fa30d4e89a369dcbc9372cd91cac80da0
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-05-28 11:52:48 +0000

    cleanup

--- src/react/src/layouts/RoleIndex.tsx
+++ src/react/src/layouts/RoleIndex.tsx
@@ -516,26 +516,6 @@ function RoleIndex({
             setFirst(0);
           }}
           dataKey="id"
-          pt={{
-            paginator: {
-              firstPageIcon: {
-                role: "img",
-                "aria-label": "First Paginator Icon",
-              },
-              prevPageIcon: {
-                role: "img",
-                "aria-label": "Previous Paginator Icon",
-              },
-              nextPageIcon: {
-                role: "img",
-                "aria-label": "Next Paginator Icon",
-              },
-              lastPageIcon: {
-                role: "img",
-                "aria-label": "Last Paginator Icon",
-              },
-            },
-          }}
         >
           <Column
             field="name"
