13607117ea7d8b1f960a3de5e2b02704b4e5d2f8
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-06-05 11:14:21 +0000

    Move Passthrough Objects to Files

---
+++ src/react/src/passthrough/DataTablePT.tsx
@@ -0,0 +1,20 @@
+export const DataTablePT = {
+  paginator: {
+    firstPageIcon: {
+      role: "img",
+      "aria-label": "First Paginator Icon",
+    },
+    prevPageIcon: {
+      role: "img",
+      "aria-label": "Previous Paginator Icon",
+    },
+    nextPageIcon: {
+      role: "img",
+      "aria-label": "Next Paginator Icon",
+    },
+    lastPageIcon: {
+      role: "img",
+      "aria-label": "Last Paginator Icon",
+    },
+  },
+};
