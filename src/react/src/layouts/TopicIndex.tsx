b299e9bd45485df50f605fdc22eb64d8f18b1529
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-07-30 14:55:23 +0000

    stash

--- src/react/src/layouts/TopicIndex.tsx
+++ src/react/src/layouts/TopicIndex.tsx
@@ -11,6 +11,8 @@ import { Messages } from "primereact/messages";
 import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
 
 import AccessButton from "../components/AccessButton";
+import EnhancedTable from "../components/EnhancedTable/components/EnhancedTable";
+import { ColumnField } from "../components/EnhancedTable/models/EnhancedTableModels";
 import SubscriberItem from "../components/SubscriberItem";
 import { Subscriber, Topic } from "../models/brewtils-types";
 import { Config, RequestItem } from "../models/models";
@@ -25,8 +27,6 @@ import {
   SyncTopics,
 } from "../services/topic_service";
 import { PaginatorTemplate } from "../services/util_service";
-import EnhancedTable from "../components/EnhancedTable/components/EnhancedTable";
-import { ColumnField } from "../components/EnhancedTable/models/EnhancedTableModels";
 
 interface TopicSubscriber {
   topic?: Topic;
@@ -45,7 +45,7 @@ function TopicIndex({
   const [topicSubscribers, setTopicSubscribers] = useState<
     Array<TopicSubscriber>
   >([]);
-  const [rawTopics, setRawTopics] = useState<Topic[]>([])
+  const [rawTopics, setRawTopics] = useState<Topic[]>([]);
   const [loading, setLoading] = useState(false);
   const [first, setFirst] = useState<number>(0);
   const [rows, setRows] = useState<number>(10);
@@ -108,7 +108,7 @@ function TopicIndex({
           },
         );
         setTopicSubscribers(topicSubscribers);
-        setRawTopics(topics)
+        setRawTopics(topics);
         setLoading(false);
       })
       .catch((error) => {
@@ -581,83 +581,84 @@ function TopicIndex({
     };
 
     const tableColumns = [
-{
-      id: "topic",
-      label: "Topic",
-      field: "name"
-    },
-    {
-      id: "publisher_count",
-      label: "Publisher Count",
-      field: "publisher_count",
-      sortable: true,
-      filterable: true,
-      isNumeric: true,
-    },
-    {
-      id: "subscribers.garden",
-      label: "Garden",
-      field: "subscribers.garden",
-      sortable: true,
-      filterable: true,
-      isString: true,
-    },
-    {
-      id: "subscribers.namespace",
-      label: "Namespace",
-      field: "subscribers.namespace",
-      sortable: true,
-      filterable: true,
-      isString: true,
-    },
-    {
-      id: "subscribers.system",
-      label: "System",
-      field: "subscribers.system",
-      sortable: true,
-      filterable: true,
-      isString: true,
-    },
-    {
-      id: "subscribers.version",
-      label: "Version",
-      field: "subscribers.version",
-      sortable: true,
-      filterable: true,
-      isString: true,
-    },
-    {
-      id: "subscribers.instance",
-      label: "Instance",
-      field: "subscribers.instance",
-      sortable: true,
-      filterable: true,
-      isString: true,
-    },
-    {
-      id: "subscribers.command",
-      label: "Command",
-      field: "subscribers.command",
-      sortable: true,
-      filterable: true,
-      isString: true,
-    },
-    {
-      id: "subscribers.consumer_count",
-      label: "Consumer Count",
-      field: "subscribers.consumer_count",
-      sortable: true,
-      filterable: true,
-      isNumeric: true,
-    },
-    {
-      id: "subscribers.subscriber_type",
-      label: "Subscriber Type",
-      field: "subscribers.subscriber_type",
-      sortable: true,
-      filterable: true,
-      isString: true,
-    },
+      {
+        id: "topic",
+        label: "Topic",
+        field: "name",
+        isString: true,
+      },
+      {
+        id: "publisher_count",
+        label: "Publisher Count",
+        field: "publisher_count",
+        sortable: true,
+        filterable: true,
+        isNumeric: true,
+      },
+      {
+        id: "subscribers.garden",
+        label: "Garden",
+        field: "subscribers.garden",
+        sortable: true,
+        filterable: true,
+        isString: true,
+      },
+      {
+        id: "subscribers.namespace",
+        label: "Namespace",
+        field: "subscribers.namespace",
+        sortable: true,
+        filterable: true,
+        isString: true,
+      },
+      {
+        id: "subscribers.system",
+        label: "System",
+        field: "subscribers.system",
+        sortable: true,
+        filterable: true,
+        isString: true,
+      },
+      {
+        id: "subscribers.version",
+        label: "Version",
+        field: "subscribers.version",
+        sortable: true,
+        filterable: true,
+        isString: true,
+      },
+      {
+        id: "subscribers.instance",
+        label: "Instance",
+        field: "subscribers.instance",
+        sortable: true,
+        filterable: true,
+        isString: true,
+      },
+      {
+        id: "subscribers.command",
+        label: "Command",
+        field: "subscribers.command",
+        sortable: true,
+        filterable: true,
+        isString: true,
+      },
+      {
+        id: "subscribers.consumer_count",
+        label: "Consumer Count",
+        field: "subscribers.consumer_count",
+        sortable: true,
+        filterable: true,
+        isNumeric: true,
+      },
+      {
+        id: "subscribers.subscriber_type",
+        label: "Subscriber Type",
+        field: "subscribers.subscriber_type",
+        sortable: true,
+        filterable: true,
+        isString: true,
+      },
     ] as ColumnField[];
 
     return (
@@ -672,8 +673,8 @@ function TopicIndex({
           // dataLength={filteredRecords}
           // totalDataLength={totalRecords}
           // reloadTable={reloadRequestsTrigger}
-          // defaultOrderBy="created_at"
-          // defaultOrder="desc"
+          defaultOrderBy="name"
+          defaultOrder="desc"
           // isLoading={loading}
         />
         <DataTable
