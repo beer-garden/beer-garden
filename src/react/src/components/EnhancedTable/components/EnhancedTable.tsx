3bf91e8785d3a67da786cee8faf720cf8ff15ec9
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-08-04 14:46:38 +0000

    Boolean Filtering Supported

--- src/react/src/components/EnhancedTable/components/EnhancedTable.tsx
+++ src/react/src/components/EnhancedTable/components/EnhancedTable.tsx
@@ -167,9 +167,20 @@ const EnhancedTable = ({
             if (typeof compare === "string") {
               compare = dayjs(new Date(compare));
             }
+          } else if (filter.isBoolean) {
+            if (typeof compare === "string") {
+              compare = compare.toLocaleLowerCase() === "true";
+            }
           }
 
           if (filter.modifier === "eq") {
+            if (filter.isBoolean) {
+              if (typeof filter.value === "string") {
+                return (
+                  (filter.value.toLocaleLowerCase() === "true") === compare
+                );
+              }
+            }
             return filter.value === compare;
           } else if (filter.modifier === "neq") {
             return filter.value !== compare;
@@ -270,6 +281,16 @@ const EnhancedTable = ({
       if (orderByField) {
         const field_a = findValue(orderByField, a);
         const field_b = findValue(orderByField, b);
+
+        if (field_a === undefined && field_b === undefined) {
+          return 0;
+        }
+        if (field_a === undefined) {
+          return order === "asc" ? 1 : -1;
+        }
+        if (field_b === undefined) {
+          return order === "asc" ? -1 : 1;
+        }
         if (
           columns.some((column) => column.field === orderBy && column.isNumeric)
         ) {
@@ -283,6 +304,17 @@ const EnhancedTable = ({
           return order === "asc"
             ? new Date(field_a).getTime() - new Date(field_b).getTime()
             : new Date(field_b).getTime() - new Date(field_a).getTime();
+        }
+        if (
+          columns.some((column) => column.field === orderBy && column.isBoolean)
+        ) {
+          if (field_a && field_b) {
+            return 0;
+          }
+          if (field_a) {
+            return order === "asc" ? 1 : -1;
+          }
+          return order === "asc" ? -1 : 1;
         }
         if (
           columns.some((column) => column.field === orderBy && column.isString)
