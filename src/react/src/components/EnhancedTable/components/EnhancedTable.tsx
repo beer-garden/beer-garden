b0f467a325a5430fbe4c89932c71971e74148e54
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-08-11 14:19:05 +0000

    Fix Table Console Logs

--- src/react/src/components/EnhancedTable/components/EnhancedTable.tsx
+++ src/react/src/components/EnhancedTable/components/EnhancedTable.tsx
@@ -33,6 +33,7 @@ const EnhancedTable = ({
   reloadTable,
   isLoading,
   displayAll,
+  keyField,
   ...props
 }: {
   data?: any[];
@@ -56,6 +57,7 @@ const EnhancedTable = ({
   reloadTable?: number;
   isLoading?: boolean;
   displayAll?: boolean;
+  keyField?: string;
 }) => {
   const [displayData, setDisplayData] = useState<any[] | undefined>(undefined);
   const [displayFiltered, setDisplayFiltered] = useState<number | undefined>(
@@ -660,9 +662,9 @@ const EnhancedTable = ({
           <TableBody>
             {displayData &&
               displayData.map((row) => (
-                <TableRow key={row.id ?? undefined}>
+                <TableRow key={ keyField ? row?.[keyField] : row.id ?? undefined}>
                   {columns.map((column) => (
-                    <TableCell>{columnData(column, row)}</TableCell>
+                    <TableCell key={`${column.id}-${keyField ? row?.[keyField] : row.id ?? undefined}`}>{columnData(column, row)}</TableCell>
                   ))}
                 </TableRow>
               ))}
@@ -709,6 +711,7 @@ const EnhancedTable = ({
         </Table>
         {(displayAll === undefined || displayAll === false) && (
           <TablePagination
+            component="div"
             rowsPerPageOptions={pageRecords ? [5, 10, 25] : []}
             colSpan={3}
             count={dataLength ?? displayDataLength}
