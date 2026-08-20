81df731c97d7cebda6ba3b2b28d14fbc8c9ffdbc
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-08-20 13:16:34 +0000

    formatting

--- src/react/src/components/RequestOptions.tsx
+++ src/react/src/components/RequestOptions.tsx
@@ -278,8 +278,11 @@ function RequestOptions({
                       <MenuList id="split-button-menu" autoFocusItem>
                         {items.map((option) => (
                           <MenuItem key={option.label} onClick={option.command}>
-                            <ListItemIcon sx={{ minWidth: "auto", mr: 1}}>
-                              <FAIcon icon={option.icon} sx={{ color: "text.primary" }}/>
+                            <ListItemIcon sx={{ minWidth: "auto", mr: 1 }}>
+                              <FAIcon
+                                icon={option.icon}
+                                sx={{ color: "text.primary" }}
+                              />
                             </ListItemIcon>
                             <ListItemText
                               primary={option.label}
