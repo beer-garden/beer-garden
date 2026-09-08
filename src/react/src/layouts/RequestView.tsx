59c11700ec3f605da3a181e46c1045d715bda4b5
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-09-08 06:32:11 -0400

    Utilize new Request API to filter response data (#2403)
    * Utilize new Request API to filter response data
    
    * Expand lazy load fields
    
    * Remove Partial Loading
    
    * Add wait for request reload
    
    * include target garden in pre cached fields

--- src/react/src/layouts/RequestView.tsx
+++ src/react/src/layouts/RequestView.tsx
@@ -25,6 +25,7 @@ function RequestView({
   const { requestId } = useParams<{ requestId: string }>();
   const urlRequestId = useRef<string | undefined>(undefined);
   const [request, setRequest] = useState<Request | undefined>(undefined);
+  const [reloadRequest, setReloadRequest] = useState(false);
 
   const [rootRequest, setRootRequest] = useState<Request | undefined>(
     undefined,
@@ -32,33 +33,16 @@ function RequestView({
 
   const rootRequestRef = useRef<Request | undefined>(undefined);
 
-  const updateRootRequest = async (request: Request) => {
-    if (request === undefined) {
-      rootRequestRef.current = undefined;
-      setRootRequest(undefined);
-    } else {
-      await loadChildrenRequests(request)
-        .then((updatedRequest) => {
-          rootRequestRef.current = updatedRequest;
-          setRootRequest(updatedRequest);
-        })
-        .catch((error) => {
-          showSnackbar({
-            severity: "error",
-            summary: "Error",
-            detail: `Error fetching children requests: ${error}`,
-            life: 3000,
-          });
-        });
-      if (
-        request.id &&
-        request.status &&
-        !["CANCELED", "SUCCESS", "ERROR", "INVALID"].includes(request.status)
-      ) {
-        if (!(request.id in listeners)) {
-          listeners[request.id] = { listener: MonitorRequestId };
-        }
-      }
+  const updateRootRequest = (request?: Request) => {
+    rootRequestRef.current = request;
+    setRootRequest(request);
+  };
+
+  const updateRequest = (request?: Request) => {
+    setRequest(request);
+    if (request !== undefined) {
+      // Force the UI to reload the request for parameters and output fields
+      setReloadRequest(true);
     }
   };
 
@@ -79,10 +63,7 @@ function RequestView({
           message.payload.id &&
           message.payload.id === rootRequestRef.current?.id
         ) {
-          updateRootRequest({
-            ...message.payload,
-            children: rootRequestRef.current?.children,
-          } as Request).catch((error) => {
+          reloadRootRequest().catch((error) => {
             showSnackbar({
               severity: "error",
               summary: "Error",
@@ -112,27 +93,15 @@ function RequestView({
             message.payload?.parent?.id &&
             matchParentId(rootRequestRef.current, message.payload.parent.id)
           ) {
-            GetRequest(rootRequestRef.current.id, {})
-              .then((updatedRootRequest) => {
-                updateRootRequest(updatedRootRequest).catch((error) => {
-                  showSnackbar({
-                    severity: "error",
-                    summary: "Error",
-                    detail: `Error Updating Root request: ${error}`,
-                    life: 3000,
-                  });
-                  setError(error);
-                });
-              })
-              .catch((error) => {
-                showSnackbar({
-                  severity: "error",
-                  summary: "Error",
-                  detail: `Error Updating Root request: ${error}`,
-                  life: 3000,
-                });
-                setError(error);
+            reloadRootRequest().catch((error) => {
+              showSnackbar({
+                severity: "error",
+                summary: "Error",
+                detail: `Error Updating Root request: ${error}`,
+                life: 3000,
               });
+              setError(error);
+            });
           }
         }
       }
@@ -140,66 +109,114 @@ function RequestView({
     [requestId],
   );
 
-  const loadChildrenRequests = async (parent_request: Request) => {
-    if (parent_request.children) {
-      const requestQueries = [];
-      const loadedChildren = [];
-      for (const childRequest of parent_request.children) {
-        if (
-          childRequest.id &&
-          (childRequest.children === undefined ||
-            childRequest.children.length === 0)
-        ) {
-          requestQueries.push(GetRequest(childRequest.id, {}));
-        } else {
-          loadedChildren.push(childRequest);
-        }
-      }
+  const loadDisplayRequest = async (update_request?: Request) => {
+    const request_id = update_request?.id ?? requestId;
 
-      if (requestQueries.length > 0) {
-        parent_request.children = [
-          ...loadedChildren,
-          ...(await Promise.all(requestQueries)),
-        ];
-      }
-      for (const childRequest of parent_request.children) {
-        await loadChildrenRequests(childRequest);
-      }
+    if (request_id === undefined) {
+      throw new Error("Unable to determine display request id");
     }
-    return parent_request;
+
+    const queryHeaders: Record<string, any> = {
+      children_depth: 0,
+      parent_depth: 0,
+    };
+
+    const data = await GetRequest(request_id, {}, queryHeaders);
+
+    setRequest(data);
+
+    setReloadRequest(false);
+  };
+
+  const reloadRootRequest = async () => {
+    if (requestId === undefined && rootRequestRef.current?.id === undefined) {
+      throw new Error("Unable to determine root request id");
+    }
+
+    let rootRequestId = rootRequestRef.current?.id;
+
+    if (rootRequestId === undefined) {
+      if (requestId === undefined) {
+        throw new Error("Unable to determine root request id");
+      }
+      const queryLookupHeaders: Record<string, any> = {
+        parent_depth: -1,
+        include: ["id", "parent"],
+      };
+      const loadedRequest = await GetRequest(requestId, {}, queryLookupHeaders);
+
+      if (loadedRequest === undefined) {
+        throw new Error("Unable to load root request");
+      }
+      const findRootParent = (request: Request) => {
+        if (request.parent !== undefined && request.parent !== null) {
+          return findRootParent(request.parent);
+        }
+        return request.id;
+      };
+
+      rootRequestId = findRootParent(loadedRequest);
+    }
+    if (rootRequestId) {
+      const queryChildrenHeaders: Record<string, any> = {
+        children_depth: -1,
+        parent_depth: 0,
+        include: [
+          "id",
+          "parent",
+          "command_type",
+          "command_display_name",
+          "command",
+          "namespace",
+          "system",
+          "system_version",
+          "instance_name",
+          "status",
+          "created_at",
+          "updated_at",
+          "status_updated_at",
+          "has_parent",
+          "target_garden",
+        ],
+      };
+
+      const root_request = await GetRequest(
+        rootRequestId,
+        {},
+        queryChildrenHeaders,
+      );
+      updateRootRequest(root_request);
+    }
   };
 
   useEffect(() => {
     if (urlRequestId.current != requestId && request !== undefined) {
       // New Page Load
       setRequest(undefined);
-      setRootRequest(undefined);
+      updateRootRequest(undefined);
       urlRequestId.current = requestId;
     } else if (!request || request.id === undefined) {
       if (requestId !== undefined) {
-        GetRequest(requestId, {})
-          .then((data: Request) => {
-            setRequest(data);
-            if (
-              !(requestId in listeners) &&
-              data.status &&
-              ["CREATED", "IN_PROGRESS"].includes(data.status)
-            ) {
-              listeners[requestId] = {
-                listener: MonitorRequestId,
-              };
-            }
-          })
-          .catch((error) => {
-            showSnackbar({
-              severity: "error",
-              summary: "Error",
-              detail: `Error fetching request: ${error}`,
-              life: 3000,
-            });
-            setError(error);
+        loadDisplayRequest().catch((error) => {
+          showSnackbar({
+            severity: "error",
+            summary: "Error",
+            detail: `Error fetching request: ${error}`,
+            life: 3000,
           });
+          setError(error);
+        });
       }
+    } else if (reloadRequest) {
+      loadDisplayRequest(request).catch((error) => {
+        showSnackbar({
+          severity: "error",
+          summary: "Error",
+          detail: `Error fetching request: ${error}`,
+          life: 3000,
+        });
+        setError(error);
+      });
     } else {
       if (
         requestId &&
@@ -226,31 +243,8 @@ function RequestView({
         };
       }
 
-      const loadRootRequest = async (check_request: Request) => {
-        if (
-          check_request.has_parent === true &&
-          check_request.parent &&
-          check_request.parent.id
-        ) {
-          const root_request = await GetRequest(check_request.parent.id, {});
-          await loadRootRequest(root_request).catch((error) => {
-            throw new error();
-          });
-        } else {
-          updateRootRequest(check_request).catch((error) => {
-            showSnackbar({
-              severity: "error",
-              summary: "Error",
-              detail: `Error Updating Root request: ${error}`,
-              life: 3000,
-            });
-            setError(error);
-          });
-        }
-      };
-
-      if (rootRequest === undefined) {
-        loadRootRequest(request).catch((error) => {
+      if (rootRequestRef.current === undefined) {
+        reloadRootRequest().catch((error) => {
           showSnackbar({
             severity: "error",
             summary: "Error",
@@ -272,7 +266,7 @@ function RequestView({
         delete listeners[rootRequestRef.current?.id];
       }
     };
-  }, [request, requestId, listeners, MonitorRequestId]);
+  }, [request, requestId, reloadRequest, listeners, MonitorRequestId]);
 
   return (
     <>
@@ -288,7 +282,7 @@ function RequestView({
               <RequestTreeMenu
                 rootRequest={rootRequest}
                 request={request}
-                setRequest={setRequest}
+                setRequest={updateRequest}
               />
             </Grid>
             <Grid size="grow">
@@ -296,7 +290,7 @@ function RequestView({
                 {request && (
                   <RequestViewMain
                     request={request}
-                    setRequest={setRequest}
+                    setRequest={updateRequest}
                     addRequestItem={addRequestItem}
                     showProjections={true}
                     config={config}
