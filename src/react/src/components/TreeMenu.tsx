290b189ae47807f4b6dc3d94918096840aa1039f
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-09-16 15:40:17 +0000

    Fix the width for borders

--- src/react/src/components/TreeMenu.tsx
+++ src/react/src/components/TreeMenu.tsx
@@ -101,6 +101,7 @@ function TreeMenu({
               selected={selected === option.id}
               onClick={() => onSelected(option)}
               sx={{
+                width: 'max-content',
                 border: (theme) =>
                   `2px solid ${theme.palette.background.default}`,
                 "&:hover, &:focus, &.Mui-focusVisible, &.Mui-selected.Mui-focusVisible, &.Mui-selected:hover":
