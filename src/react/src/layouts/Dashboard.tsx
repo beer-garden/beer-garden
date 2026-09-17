e2808cec8e0172fbf5e2a89b3873edf4c764d0fb
1maple1 <160027655+1maple1@users.noreply.github.com>
2026-09-17 04:24:15 -0400

    Change overflowX style from 'scroll' to 'auto'

--- src/react/src/layouts/Dashboard.tsx
+++ src/react/src/layouts/Dashboard.tsx
@@ -604,7 +604,7 @@ function GardenDashboard({
               border: "1px solid",
               borderColor: grey[300],
               borderRadius: 2,
-              overflowX: "scroll"
+              overflowX: "auto"
             }}
             {...GenerateTourProps(gardenTreeTourStep)}
             treeItems={gardenMenu ?? []}
