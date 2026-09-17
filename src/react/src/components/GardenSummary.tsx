b38b19f12c2ac04311dd15bf14155c3e6d039551
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-09-17 19:52:19 +0000

    Fix Parent Routing Check

--- src/react/src/components/GardenSummary.tsx
+++ src/react/src/components/GardenSummary.tsx
@@ -200,6 +200,12 @@ function GardenSummary({
     setReceivingonnections(getReceivingConnections());
     setSystemCounts(getSystemCounts());
 
+    if (gardenRef.current && selectedGarden?.parent) {
+      parentRoutingCheck(gardenRef.current, selectedGarden?.parent, true);
+    } else {
+      setInvalidRouting(false)
+    }
+
     if (tourStepsRef !== undefined) {
       ClearTourSteps(tourStepsRef, tourPrefix, tourUuid);
 
@@ -210,10 +216,7 @@ function GardenSummary({
       if (gardenRef.current) {
         if (gardenRef.current.name === selectedGarden?.name) {
           AddTourStep(tourStepsRef, syncAllTourStep);
-        } else {
-          if (selectedGarden?.parent) {
-            parentRoutingCheck(gardenRef.current, selectedGarden?.parent, true);
-          }
+        } else {        
           AddTourStep(tourStepsRef, syncGardenTourStep);
           AddTourStep(tourStepsRef, syncUsersTourStep);
           AddTourStep(tourStepsRef, deleteGardenTourStep);
